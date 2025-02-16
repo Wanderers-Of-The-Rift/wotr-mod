d886ae58d0ec60e1068b7e83ffa10613658daa52
McMelonTV <mcmelontube@gmail.com>
2025-02-16 14:16:12 +0100

    feat: add all runegem shapes to the creative menu

--- src/main/java/com/dimensiondelvers/dimensiondelvers/init/ModCreativeTabs.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/init/ModCreativeTabs.java
@@ -1,10 +1,14 @@
 package com.dimensiondelvers.dimensiondelvers.init;
 
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import com.dimensiondelvers.dimensiondelvers.item.runegem.RunegemData;
+import com.dimensiondelvers.dimensiondelvers.item.runegem.RunegemShape;
+import com.dimensiondelvers.dimensiondelvers.item.runegem.RunegemTier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -23,7 +27,29 @@ public class ModCreativeTabs {
                                 output.accept(ModItems.DEV_BLOCK_ITEM);
                                 output.accept(ModItems.RUNE_ANVIL_BLOCK_ITEM);
 
-                                output.accept(ModItems.RUNEGEM);
+                                ItemStack circleGem = ModItems.RUNEGEM.toStack();
+                                circleGem.set(ModDataComponentType.RUNEGEM_DATA, new RunegemData(RunegemShape.CIRCLE, DimensionDelvers.tagId(ModModifiers.MODIFIER_KEY, "raw_attack_rune"), RunegemTier.RAW));
+                                output.accept(circleGem);
+
+                                ItemStack squareGem = ModItems.RUNEGEM.toStack();
+                                squareGem.set(ModDataComponentType.RUNEGEM_DATA, new RunegemData(RunegemShape.SQUARE, DimensionDelvers.tagId(ModModifiers.MODIFIER_KEY, "raw_attack_rune"), RunegemTier.RAW));
+                                output.accept(squareGem);
+
+                                ItemStack triangleGem = ModItems.RUNEGEM.toStack();
+                                triangleGem.set(ModDataComponentType.RUNEGEM_DATA, new RunegemData(RunegemShape.TRIANGLE, DimensionDelvers.tagId(ModModifiers.MODIFIER_KEY, "raw_attack_rune"), RunegemTier.RAW));
+                                output.accept(triangleGem);
+
+                                ItemStack diamondGem = ModItems.RUNEGEM.toStack();
+                                diamondGem.set(ModDataComponentType.RUNEGEM_DATA, new RunegemData(RunegemShape.DIAMOND, DimensionDelvers.tagId(ModModifiers.MODIFIER_KEY, "raw_attack_rune"), RunegemTier.RAW));
+                                output.accept(diamondGem);
+
+                                ItemStack heartGem = ModItems.RUNEGEM.toStack();
+                                heartGem.set(ModDataComponentType.RUNEGEM_DATA, new RunegemData(RunegemShape.HEART, DimensionDelvers.tagId(ModModifiers.MODIFIER_KEY, "raw_attack_rune"), RunegemTier.RAW));
+                                output.accept(heartGem);
+
+                                ItemStack pentagonGem = ModItems.RUNEGEM.toStack();
+                                pentagonGem.set(ModDataComponentType.RUNEGEM_DATA, new RunegemData(RunegemShape.PENTAGON, DimensionDelvers.tagId(ModModifiers.MODIFIER_KEY, "raw_attack_rune"), RunegemTier.RAW));
+                                output.accept(pentagonGem);
                             }).build());
 
 }
