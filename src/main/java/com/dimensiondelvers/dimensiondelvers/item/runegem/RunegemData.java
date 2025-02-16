c9a4642c65db99c9e6c51048b5ee5a9910ec7648
Patrigan <patrick.provinciael@gmail.com>
2025-02-16 16:01:07 +0100

    Fix: Weird dragging bug is fixed.

--- src/main/java/com/dimensiondelvers/dimensiondelvers/item/runegem/RunegemData.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/item/runegem/RunegemData.java
@@ -1,8 +1,14 @@
 package com.dimensiondelvers.dimensiondelvers.item.runegem;
 
+import com.dimensiondelvers.dimensiondelvers.init.ModModifiers;
+import com.dimensiondelvers.dimensiondelvers.modifier.Modifier;
+import com.dimensiondelvers.dimensiondelvers.modifier.ModifierInstance;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.Level;
 
@@ -11,9 +17,19 @@ import java.util.Optional;
 public record RunegemData(RunegemShape shape, TagKey<Modifier> tag, RunegemTier tier) {
     public static Codec<RunegemData> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             RunegemShape.CODEC.fieldOf("shape").forGetter(RunegemData::shape),
-            TagKey.codec(Registries.ENCHANTMENT).fieldOf("tag").forGetter(RunegemData::tag),
+            TagKey.codec(ModModifiers.MODIFIER_KEY).fieldOf("tag").forGetter(RunegemData::tag),
             RunegemTier.CODEC.fieldOf("tier").forGetter(RunegemData::tier)
     ).apply(inst, RunegemData::new));
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, RunegemData> STREAM_CODEC = StreamCodec.composite(
+            RunegemShape.STREAM_CODEC,
+            RunegemData::shape,
+            TagKey.streamCodec(ModModifiers.MODIFIER_KEY),
+            RunegemData::tag,
+            RunegemTier.STREAM_CODEC,
+            RunegemData::tier,
+            RunegemData::new
+    );
 
     public Optional<Holder<Modifier>> getRandomModifier(Level level) {
         return level.registryAccess().lookupOrThrow(ModModifiers.MODIFIER_KEY)
--- src/main/java/com/dimensiondelvers/dimensiondelvers/item/runegem/RunegemTier.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/item/runegem/RunegemTier.java
@@ -3,26 +3,40 @@ package com.dimensiondelvers.dimensiondelvers.item.runegem;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.util.ByIdMap;
+
+import java.util.function.IntFunction;
 
 public enum RunegemTier {
 
-    RAW("raw"),
-    SHAPED("shaped"),
-    CUT("cut"),
-    POLISHED("polished"),
-    FRAMED("framed"),
-    UNIQUE("unique");
+    RAW(0, "raw"),
+    SHAPED(1, "shaped"),
+    CUT(2, "cut"),
+    POLISHED(3, "polished"),
+    FRAMED(4, "framed"),
+    UNIQUE(5, "unique");
 
     public static final Codec<RunegemTier> CODEC = Codec.STRING.flatComapMap(s -> RunegemTier.byName(s, null), d -> DataResult.success(d.getName()));
+    public static final IntFunction<RunegemTier> BY_ID = ByIdMap.continuous(RunegemTier::getId, values(), ByIdMap.OutOfBoundsStrategy.WRAP);
+    public static final StreamCodec<ByteBuf, RunegemTier> STREAM_CODEC = ByteBufCodecs.idMapper(BY_ID, RunegemTier::getId);
 
+    private final int id;
     private final String name;
 
-    private RunegemTier(String name) {
+    private RunegemTier(int id, String name) {
+        this.id = id;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public static RunegemTier byName(String name, RunegemTier defaultReturn) {
