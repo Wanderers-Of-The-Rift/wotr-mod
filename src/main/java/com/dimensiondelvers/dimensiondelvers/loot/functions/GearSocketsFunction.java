a333673ee0425f86b9187982a74de3024c5a7e4a
Patrigan <patrick.provinciael@gmail.com>
2025-02-18 00:32:38 +0100

    Feature: Added a GearSocketsLootFunction
    Unsure yet how to use this to make dropped gear spawn with sockets, though

---
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/loot/functions/GearSocketsFunction.java
@@ -0,0 +1,63 @@
+package com.dimensiondelvers.dimensiondelvers.loot.functions;
+
+import com.dimensiondelvers.dimensiondelvers.init.ModDataComponentType;
+import com.dimensiondelvers.dimensiondelvers.item.socket.GearSocket;
+import com.dimensiondelvers.dimensiondelvers.item.socket.GearSockets;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.functions.LootItemConditionalFunction;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.dimensiondelvers.dimensiondelvers.init.ModLootItemFunctionTypes.GEAR_SOCKETS_FUNCTION;
+
+public class GearSocketsFunction extends LootItemConditionalFunction {
+    public static final MapCodec<GearSocketsFunction> CODEC = RecordCodecBuilder.mapCodec(
+            inst -> commonFields(inst)
+                    .and(
+                            Codec.INT.fieldOf("max_sockets").forGetter(GearSocketsFunction::getMaxSockets)
+                    )
+                    .apply(inst, GearSocketsFunction::new)
+    );
+
+    private final int maxSockets;
+
+    protected GearSocketsFunction(List<LootItemCondition> predicates, int maxSockets) {
+        super(predicates);
+        this.maxSockets = maxSockets;
+    }
+
+    public int getMaxSockets() {
+        return maxSockets;
+    }
+
+    @Override
+    public LootItemFunctionType<? extends LootItemConditionalFunction> getType() {
+        return GEAR_SOCKETS_FUNCTION.get();
+    }
+
+    @Override
+    protected ItemStack run(ItemStack itemStack, LootContext lootContext) {
+        return generateItemStack(itemStack, lootContext.getRandom());
+    }
+
+    private @NotNull ItemStack generateItemStack(ItemStack itemStack, RandomSource random) {
+        List<GearSocket> sockets = new ArrayList<>();
+        for (int i = 0; i < random.nextInt(maxSockets + 1); i++) {
+            GearSocket socket = GearSocket.getRandomSocket(random);
+            if (socket != null) {
+                sockets.add(socket);
+            }
+        }
+        itemStack.set(ModDataComponentType.GEAR_SOCKETS, new GearSockets(sockets));
+        return itemStack;
+    }
+}
