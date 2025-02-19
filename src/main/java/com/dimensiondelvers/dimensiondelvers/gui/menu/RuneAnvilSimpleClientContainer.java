adda54a040c8d7de4b661e69a37c7fe97d98ae34
McMelonTV <mcmelontube@gmail.com>
2025-02-19 05:12:56 +0100

    fix(wip): kinda fix a part of the rune anvil block entity but its still duping runegems

---
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/gui/menu/RuneAnvilSimpleClientContainer.java
@@ -0,0 +1,9 @@
+package com.dimensiondelvers.dimensiondelvers.gui.menu;
+
+import net.minecraft.world.SimpleContainer;
+
+public class RuneAnvilSimpleClientContainer extends SimpleContainer implements RuneAnvilContainer {
+    public RuneAnvilSimpleClientContainer() {
+        super(SIZE);
+    }
+}
