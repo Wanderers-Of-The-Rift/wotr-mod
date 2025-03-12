2957ed6b476085af2cf8ffc9335d933f6cd31c92
Patrigan <patrick.provinciael@gmail.com>
2025-03-12 23:26:35 +0100

    Feature: JigsawBlockRenderer for Nametag

--- src/main/java/com/wanderersoftherift/wotr/events/client/ClientRegistryEvents.java
+++ src/main/java/com/wanderersoftherift/wotr/events/client/ClientRegistryEvents.java
@@ -1,12 +1,15 @@
 package com.wanderersoftherift.wotr.events.client;
 
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import com.wanderersoftherift.wotr.client.render.blockentity.JigsawBlockEntityRenderer;
 import com.wanderersoftherift.wotr.client.render.item.properties.select.SelectRuneGemShape;
 import com.wanderersoftherift.wotr.client.tooltip.ImageTooltipRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 
@@ -21,5 +24,13 @@ public class ClientRegistryEvents {
     @SubscribeEvent
     public static void registerClientTooltipComponents(RegisterClientTooltipComponentFactoriesEvent event) {
         event.register(ImageTooltipRenderer.ImageComponent.class, ImageTooltipRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(
+                BlockEntityType.JIGSAW,
+                JigsawBlockEntityRenderer::new
+        );
     }
 }
