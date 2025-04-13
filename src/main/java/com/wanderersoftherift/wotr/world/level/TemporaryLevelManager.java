Dimension-Delvers/dimension-delvers-mod

feat: respawn on portal

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/TemporaryLevelManager.java
@@ -2,13 +2,12 @@ package com.dimensiondelvers.dimensiondelvers.world.level;
 
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
 import com.dimensiondelvers.dimensiondelvers.init.ModBlocks;
-import com.dimensiondelvers.dimensiondelvers.mixin.AccessorMinecraftServer;
 import com.dimensiondelvers.dimensiondelvers.network.S2CLevelListUpdatePacket;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
 import net.minecraft.core.MappedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biomes;
@@ -26,15 +25,16 @@ import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public class TemporaryLevelManager {
     private static final List<TemporaryLevel> levels = new ArrayList<>();
 
-    // TODO: persistent dimensions (DO NOT SHUT DOWN THE SERVER IN RIFT - you won't be able to rejoin)
+    // TODO: persistent rifts - rift isn't a TemporaryLevel after reload now
     // TODO: deregister and delete files after exiting rift
-    public static TemporaryLevel createRiftLevel() {
-        ResourceLocation id = DimensionDelvers.id("rift_" + UUID.randomUUID());
+    @SuppressWarnings("deprecation")
+    public static TemporaryLevel createRiftLevel(ResourceKey<Level> portalDimension, BlockPos portalPos) {
+        // Blockpos until we store dimension id in the portal
+        ResourceLocation id = DimensionDelvers.id("rift_" + portalPos.getX() + "_" + portalPos.getY() + "_" + portalPos.getZ()/* UUID.randomUUID()*/);
 
         var ow = ServerLifecycleHooks.getCurrentServer().overworld();
         Optional<Registry<LevelStem>> levelStemRegistry = ow.registryAccess().lookup(Registries.LEVEL_STEM);
@@ -51,43 +51,44 @@ public class TemporaryLevelManager {
             return null;
         }
 
-        //TODO: figure out how to use the dimension type
-        var stem = new LevelStem(Holder.direct(ow.dimensionType()),chunkGen);
+        var riftType =  ServerLifecycleHooks.getCurrentServer().registryAccess().lookupOrThrow(Registries.DIMENSION_TYPE).get(RiftDimensionType.RIFT_DIMENSION_TYPE).get();
+        var stem = new LevelStem(riftType, chunkGen);
         var stemRegistry = levelStemRegistry.get();
         if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
             mappedStemRegistry.unfreeze(false);
-        }
-        Registry.register(stemRegistry, id, stem);
-
-        if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
+            if (stemRegistry.get(id).isEmpty()) {
+                Registry.register(stemRegistry, id, stem);
+            }
             mappedStemRegistry.freeze();
         }
 
-        TemporaryLevel level = TemporaryLevel.create(id, stem);
+
+        TemporaryLevel level = TemporaryLevel.create(id, stem, portalDimension, portalPos);
 
         Registry<Level> registry = dimensionRegistry.get();
         if (registry instanceof MappedRegistry<Level> mappedRegistry) {
             mappedRegistry.unfreeze(false);
-        }
-        if (registry.get(id).isEmpty()){
-            Registry.register(registry, id, level);
-        }
-        if (registry instanceof MappedRegistry<Level> mappedRegistry) {
+            if (registry.get(id).isEmpty()) {
+                Registry.register(registry, id, level);
+            }
             mappedRegistry.freeze();
+
         }
 
-        ((AccessorMinecraftServer)level.getServer()).getLevels().put(level.dimension(), level);
+        level.getServer().forgeGetWorldMap().put(level.dimension(), level);
         level.getServer().markWorldsDirty();
         NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, false));
         TemporaryLevelManager.levels.add(level);
-        level.setBlock(new BlockPos(0,-64,0), ModBlocks.RIFT_PORTAL_BLOCK.get().defaultBlockState(), 3);
+        level.setBlock(new BlockPos(0, -64, 0), ModBlocks.RIFT_PORTAL_BLOCK.get().defaultBlockState(), 3);
         return level;
     }
 
+    @SuppressWarnings("deprecation")
     public static void unregisterLevel(TemporaryLevel level) {
-        // idk how to deregister stuff
-        ((AccessorMinecraftServer)level.getServer()).getLevels().remove(level.dimension());
+        level.save(null, true, false);
+        //TODO: what to do with LevelStem?
+        level.getServer().forgeGetWorldMap().remove(level);
         NeoForge.EVENT_BUS.post(new LevelEvent.Unload(level));
         ResourceLocation id = level.getId();
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, true));
@@ -98,7 +99,7 @@ public class TemporaryLevelManager {
     public static void deleteLevel(TemporaryLevel level) {
     }
 
-    private static ChunkGenerator getRiftChunkGenerator(){
+    private static ChunkGenerator getRiftChunkGenerator() {
         var biomeHolder = ServerLifecycleHooks.getCurrentServer().overworld().registryAccess().lookup(Registries.BIOME).flatMap(x -> x.get(Biomes.THE_VOID)).orElse(null);
         if (biomeHolder == null) {
             return null;

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/world/level/TemporaryLevelManager.java
@@ -3,12 +3,11 @@ package com.wanderersoftherift.wotr.world.level;
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import com.wanderersoftherift.wotr.init.ModBlocks;
 import com.wanderersoftherift.wotr.network.S2CLevelListUpdatePacket;
-import com.wanderersoftherift.wotr.mixin.AccessorMinecraftServer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
 import net.minecraft.core.MappedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biomes;
@@ -26,15 +25,16 @@ import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public class TemporaryLevelManager {
     private static final List<TemporaryLevel> levels = new ArrayList<>();
 
-    // TODO: persistent dimensions (DO NOT SHUT DOWN THE SERVER IN RIFT - you won't be able to rejoin)
+    // TODO: persistent rifts - rift isn't a TemporaryLevel after reload now
     // TODO: deregister and delete files after exiting rift
-    public static TemporaryLevel createRiftLevel() {
-        ResourceLocation id = WanderersOfTheRift.id("rift_" + UUID.randomUUID());
+    @SuppressWarnings("deprecation")
+    public static TemporaryLevel createRiftLevel(ResourceKey<Level> portalDimension, BlockPos portalPos) {
+        // Blockpos until we store dimension id in the portal
+        ResourceLocation id = WanderersOfTheRift.id("rift_" + portalPos.getX() + "_" + portalPos.getY() + "_" + portalPos.getZ()/* UUID.randomUUID()*/);
 
         var ow = ServerLifecycleHooks.getCurrentServer().overworld();
         Optional<Registry<LevelStem>> levelStemRegistry = ow.registryAccess().lookup(Registries.LEVEL_STEM);
@@ -51,53 +51,55 @@ public class TemporaryLevelManager {
             return null;
         }
 
-        //TODO: figure out how to use the dimension type
-        var stem = new LevelStem(Holder.direct(ow.dimensionType()),chunkGen);
+        var riftType =  ServerLifecycleHooks.getCurrentServer().registryAccess().lookupOrThrow(Registries.DIMENSION_TYPE).get(RiftDimensionType.RIFT_DIMENSION_TYPE).get();
+        var stem = new LevelStem(riftType, chunkGen);
         var stemRegistry = levelStemRegistry.get();
         if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
             mappedStemRegistry.unfreeze(false);
-        }
-        Registry.register(stemRegistry, id, stem);
-
-        if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
+            if (stemRegistry.get(id).isEmpty()) {
+                Registry.register(stemRegistry, id, stem);
+            }
             mappedStemRegistry.freeze();
         }
 
-        TemporaryLevel level = TemporaryLevel.create(id, stem);
+
+        TemporaryLevel level = TemporaryLevel.create(id, stem, portalDimension, portalPos);
 
         Registry<Level> registry = dimensionRegistry.get();
         if (registry instanceof MappedRegistry<Level> mappedRegistry) {
             mappedRegistry.unfreeze(false);
-        }
-        if (registry.get(id).isEmpty()){
-            Registry.register(registry, id, level);
-        }
-        if (registry instanceof MappedRegistry<Level> mappedRegistry) {
+            if (registry.get(id).isEmpty()) {
+                Registry.register(registry, id, level);
+            }
             mappedRegistry.freeze();
+
         }
 
-        ((AccessorMinecraftServer)level.getServer()).getLevels().put(level.dimension(), level);
+        level.getServer().forgeGetWorldMap().put(level.dimension(), level);
         level.getServer().markWorldsDirty();
         NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, false));
         TemporaryLevelManager.levels.add(level);
-        level.setBlock(new BlockPos(0,-64,0), ModBlocks.RIFT_PORTAL_BLOCK.get().defaultBlockState(), 3);
+        level.setBlock(new BlockPos(0, -64, 0), ModBlocks.RIFT_PORTAL_BLOCK.get().defaultBlockState(), 3);
         return level;
     }
 
+    @SuppressWarnings("deprecation")
     public static void unregisterLevel(TemporaryLevel level) {
-        // idk how to deregister stuff
-        ((AccessorMinecraftServer)level.getServer()).getLevels().remove(level.dimension());
+        level.save(null, true, false);
+        //TODO: what to do with LevelStem?
+        level.getServer().forgeGetWorldMap().remove(level);
         NeoForge.EVENT_BUS.post(new LevelEvent.Unload(level));
         ResourceLocation id = level.getId();
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, true));
+        level.getServer().markWorldsDirty();
         TemporaryLevelManager.levels.remove(level);
     }
 
     public static void deleteLevel(TemporaryLevel level) {
     }
 
-    private static ChunkGenerator getRiftChunkGenerator(){
+    private static ChunkGenerator getRiftChunkGenerator() {
         var biomeHolder = ServerLifecycleHooks.getCurrentServer().overworld().registryAccess().lookup(Registries.BIOME).flatMap(x -> x.get(Biomes.THE_VOID)).orElse(null);
         if (biomeHolder == null) {
             return null;