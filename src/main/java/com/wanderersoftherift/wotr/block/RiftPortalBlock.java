Dimension-Delvers/dimension-delvers-mod

fix: rifts work properly after server restart

src/main/java/com/dimensiondelvers/dimensiondelvers/block/RiftPortalBlock.java
@@ -31,24 +31,31 @@ public class RiftPortalBlock extends Block {
             return InteractionResult.SUCCESS;
         } else {
             if (level instanceof TemporaryLevel temporaryLevel) {
-                if (player instanceof ServerPlayer serverPlayer){
+                if (player instanceof ServerPlayer serverPlayer) {
                     TemporaryLevelManager.unregisterLevel(temporaryLevel);
 
                     ResourceKey<Level> respawnKey = temporaryLevel.getPortalDimension();
-                    if (respawnKey == level.dimension()){
+                    if (respawnKey == level.dimension()) {
                         respawnKey = Level.OVERWORLD;
                     }
                     ServerLevel respawnDimension = level.getServer().getLevel(respawnKey);
-                    var respawnPos = temporaryLevel.getPortalPos().above();
+                    if (respawnDimension == null) {
+                       respawnDimension = level.getServer().overworld();
+                    }
+                    var portalPos = temporaryLevel.getPortalPos();
+                    var respawnPos = respawnDimension.getSharedSpawnPos();
+                    if (portalPos != null) {
+                        respawnPos = portalPos.above();
+                    }
 
                     player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
-                } else{
+                } else {
                     player.displayClientMessage(Component.literal("Failed to create rift"), true);
                 }
 
                 return InteractionResult.SUCCESS;
             }
-            if(level.dimension().location().getNamespace().equals("dimensiondelvers")) {
+            if (level.dimension().location().getNamespace().equals("dimensiondelvers")) {
                 if (player instanceof ServerPlayer serverPlayer) {
                     player.displayClientMessage(Component.literal("You're in inactive rift - teleporting to respawn pos"), true);
                     ResourceKey<Level> respawnKey = serverPlayer.getRespawnDimension();
@@ -58,7 +65,7 @@ public class RiftPortalBlock extends Block {
                         respawnPos = null;
                     }
                     ServerLevel respawnDimension = level.getServer().getLevel(respawnKey);
-                    if (respawnPos == null){
+                    if (respawnPos == null) {
                         respawnPos = respawnDimension.getSharedSpawnPos();
                     }
                     player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);

src/main/java/com/dimensiondelvers/dimensiondelvers/mixin/MixinMinecraftServer.java
@@ -1,0 +1,46 @@
+package com.dimensiondelvers.dimensiondelvers.mixin;
+
+import com.dimensiondelvers.dimensiondelvers.world.level.TemporaryLevel;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mixin(MinecraftServer.class)
+public abstract class MixinMinecraftServer {
+
+    @Shadow public abstract Map<ResourceKey<Level>, ServerLevel> forgeGetWorldMap();
+
+
+    // This will be injected when overworld is already loaded, but nothing else is
+    // It will split the dimension set into 2 - rifts and nonRifts
+    // It will then create TemporaryLevel for each rift and load it
+    // nonRifts will be passed back for normal loading
+    @WrapOperation(method = "createLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;entrySet()Ljava/util/Set;"))
+    private Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>> createRift(Registry<LevelStem> instance, Operation<Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>>> original){
+        var originalSet = original.call(instance);
+        var rifts = originalSet.stream().filter(e -> e.getKey().location().getNamespace().equals("dimensiondelvers")).toList();
+        var nonRifts = originalSet.stream().filter(e -> !e.getKey().location().getNamespace().equals("dimensiondelvers")).collect(Collectors.toSet());
+        for (Map.Entry<ResourceKey<LevelStem>, LevelStem> rift : rifts){
+            var level = TemporaryLevel.create(rift.getKey().location(), rift.getValue(), null, null);
+            this.forgeGetWorldMap().put(ResourceKey.create(Registries.DIMENSION, rift.getKey().location()), level);
+            NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
+        }
+        return nonRifts;
+    }
+
+}

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/block/RiftPortalBlock.java
@@ -31,34 +31,41 @@ public class RiftPortalBlock extends Block {
             return InteractionResult.SUCCESS;
         } else {
             if (level instanceof TemporaryLevel temporaryLevel) {
-                if (player instanceof ServerPlayer serverPlayer){
+                if (player instanceof ServerPlayer serverPlayer) {
                     TemporaryLevelManager.unregisterLevel(temporaryLevel);
 
                     ResourceKey<Level> respawnKey = temporaryLevel.getPortalDimension();
-                    if (respawnKey == level.dimension()){
+                    if (respawnKey == level.dimension()) {
                         respawnKey = Level.OVERWORLD;
                     }
                     ServerLevel respawnDimension = level.getServer().getLevel(respawnKey);
-                    var respawnPos = temporaryLevel.getPortalPos().above();
+                    if (respawnDimension == null) {
+                       respawnDimension = level.getServer().overworld();
+                    }
+                    var portalPos = temporaryLevel.getPortalPos();
+                    var respawnPos = respawnDimension.getSharedSpawnPos();
+                    if (portalPos != null) {
+                        respawnPos = portalPos.above();
+                    }
 
                     player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
-                } else{
+                } else {
                     player.displayClientMessage(Component.literal("Failed to create rift"), true);
                 }
 
                 return InteractionResult.SUCCESS;
             }
-            if(level.dimension().location().getNamespace().equals("dimensiondelvers")) {
+            if (level.dimension().location().getNamespace().equals("wotr")) {
                 if (player instanceof ServerPlayer serverPlayer) {
                     player.displayClientMessage(Component.literal("You're in inactive rift - teleporting to respawn pos"), true);
                     ResourceKey<Level> respawnKey = serverPlayer.getRespawnDimension();
                     var respawnPos = serverPlayer.getRespawnPosition();
-                    if (respawnKey.location().getNamespace().equals("dimensiondelvers")) {
+                    if (respawnKey.location().getNamespace().equals("wotr")) {
                         respawnKey = Level.OVERWORLD;
                         respawnPos = null;
                     }
                     ServerLevel respawnDimension = level.getServer().getLevel(respawnKey);
-                    if (respawnPos == null){
+                    if (respawnPos == null) {
                         respawnPos = respawnDimension.getSharedSpawnPos();
                     }
                     player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);

src/main/java/com/wanderersoftherift/wotr/mixin/MixinMinecraftServer.java
@@ -1,0 +1,46 @@
+package com.wanderersoftherift.wotr.mixin;
+
+import com.wanderersoftherift.wotr.world.level.TemporaryLevel;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mixin(MinecraftServer.class)
+public abstract class MixinMinecraftServer {
+
+    @Shadow public abstract Map<ResourceKey<Level>, ServerLevel> forgeGetWorldMap();
+
+
+    // This will be injected when overworld is already loaded, but nothing else is
+    // It will split the dimension set into 2 - rifts and nonRifts
+    // It will then create TemporaryLevel for each rift and load it
+    // nonRifts will be passed back for normal loading
+    @WrapOperation(method = "createLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;entrySet()Ljava/util/Set;"))
+    private Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>> createRift(Registry<LevelStem> instance, Operation<Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>>> original){
+        var originalSet = original.call(instance);
+        var rifts = originalSet.stream().filter(e -> e.getKey().location().getNamespace().equals("wotr")).toList();
+        var nonRifts = originalSet.stream().filter(e -> !e.getKey().location().getNamespace().equals("wotr")).collect(Collectors.toSet());
+        for (Map.Entry<ResourceKey<LevelStem>, LevelStem> rift : rifts){
+            var level = TemporaryLevel.create(rift.getKey().location(), rift.getValue(), null, null);
+            this.forgeGetWorldMap().put(ResourceKey.create(Registries.DIMENSION, rift.getKey().location()), level);
+            NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
+        }
+        return nonRifts;
+    }
+
+}