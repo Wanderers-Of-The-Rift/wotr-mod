Dimension-Delvers/dimension-delvers-mod

feat: basic in-memory runtime dimensions

src/main/java/com/dimensiondelvers/dimensiondelvers/mixin/AccessorMinecraftServer.java
@@ -1,0 +1,40 @@
+package com.dimensiondelvers.dimensiondelvers.mixin;
+
+import net.minecraft.core.LayeredRegistryAccess;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.RegistryLayer;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.WorldData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+import java.util.concurrent.Executor;
+
+
+@Mixin(MinecraftServer.class)
+public interface AccessorMinecraftServer {
+    @Accessor
+    LayeredRegistryAccess<RegistryLayer> getRegistries();
+
+    @Accessor
+    Executor getExecutor();
+
+    @Accessor
+    LevelStorageSource.LevelStorageAccess getStorageSource();
+
+    @Accessor
+    WorldData getWorldData();
+
+    @Accessor
+    ChunkProgressListenerFactory getProgressListenerFactory();
+
+    @Accessor
+    Map<ResourceKey<Level>, ServerLevel> getLevels();
+}

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/TemporaryLevel.java
@@ -1,19 +1,58 @@
 package com.dimensiondelvers.dimensiondelvers.world.level;
 
+import com.dimensiondelvers.dimensiondelvers.mixin.AccessorMinecraftServer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.world.RandomSequences;
+import net.minecraft.world.level.CustomSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.storage.DerivedLevelData;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.ServerLevelData;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+import java.util.List;
+import java.util.concurrent.Executor;
 
 public class TemporaryLevel extends ServerLevel {
     private final ResourceLocation id;
 
-    public TemporaryLevel(ResourceLocation id) {
-        super(
-                ServerLifecycleHooks.getCurrentServer(),
-                // ???
-        );
-
+    private TemporaryLevel(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess,
+                           ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener,
+                           boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime,
+                           RandomSequences randomSequences, ResourceLocation id) {
+        super(server, dispatcher, levelStorageAccess, serverLevelData, dimension, levelStem, progressListener, isDebug, biomeZoomSeed, customSpawners, tickTime, randomSequences);
         this.id = id;
+    }
+
+
+    public static TemporaryLevel create(ResourceLocation id, LevelStem stem) {
+        AccessorMinecraftServer server = (AccessorMinecraftServer) ServerLifecycleHooks.getCurrentServer();
+        var chunkProgressListener = server.getProgressListenerFactory().create(0);
+        var storageSource = server.getStorageSource();
+        var worldData = server.getWorldData();
+        var executor = server.getExecutor();
+
+        return new TemporaryLevel(
+            ServerLifecycleHooks.getCurrentServer(),
+            executor,
+            storageSource,
+            new DerivedLevelData(worldData, worldData.overworldData()),
+            ResourceKey.create(Registries.DIMENSION, id),
+            stem,
+            chunkProgressListener,
+            false,
+            0L,
+            List.of(),
+            false,
+            ServerLifecycleHooks.getCurrentServer().overworld().getRandomSequences(),
+            id
+        );
     }
 
     public ResourceLocation getId() {

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/TemporaryLevelManager.java
@@ -1,12 +1,27 @@
 package com.dimensiondelvers.dimensiondelvers.world.level;
 
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import com.dimensiondelvers.dimensiondelvers.init.ModBlocks;
+import com.dimensiondelvers.dimensiondelvers.mixin.AccessorMinecraftServer;
 import com.dimensiondelvers.dimensiondelvers.network.S2CLevelListUpdatePacket;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.MappedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.levelgen.flat.FlatLayerInfo;
+import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,30 +31,81 @@ import java.util.UUID;
 public class TemporaryLevelManager {
     private static final List<TemporaryLevel> levels = new ArrayList<>();
 
+    // TODO: persistent dimensions (DO NOT SHUT DOWN THE SERVER IN RIFT - you won't be able to rejoin)
+    // TODO: deregister and delete files after exiting rift
     public static TemporaryLevel createRiftLevel() {
         ResourceLocation id = DimensionDelvers.id("rift_" + UUID.randomUUID());
 
-        TemporaryLevel level = new TemporaryLevel(id);
-        registerLevel(level);
-        return level;
-    }
-
-    private static void registerLevel(TemporaryLevel level) {
-        Optional<Registry<Level>> dimensionRegistry = level.registryAccess().lookup(Registries.DIMENSION);
-        if (dimensionRegistry.isEmpty()) {
-            return;
+        var ow = ServerLifecycleHooks.getCurrentServer().overworld();
+        Optional<Registry<LevelStem>> levelStemRegistry = ow.registryAccess().lookup(Registries.LEVEL_STEM);
+        if (levelStemRegistry.isEmpty()) {
+            return null;
         }
-        ResourceLocation id = level.getId();
+        Optional<Registry<Level>> dimensionRegistry = ow.registryAccess().lookup(Registries.DIMENSION);
+        if (dimensionRegistry.isEmpty()) {
+            return null;
+        }
+
+        ChunkGenerator chunkGen = getRiftChunkGenerator();
+        if (chunkGen == null) {
+            return null;
+        }
+
+        //TODO: figure out how to use the dimension type
+        var stem = new LevelStem(Holder.direct(ow.dimensionType()),chunkGen);
+        var stemRegistry = levelStemRegistry.get();
+        if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
+            mappedStemRegistry.unfreeze(false);
+        }
+        Registry.register(stemRegistry, id, stem);
+
+        if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
+            mappedStemRegistry.freeze();
+        }
+
+        TemporaryLevel level = TemporaryLevel.create(id, stem);
+
         Registry<Level> registry = dimensionRegistry.get();
-        Registry.register(registry, id, level);
+        if (registry instanceof MappedRegistry<Level> mappedRegistry) {
+            mappedRegistry.unfreeze(false);
+        }
+        if (registry.get(id).isEmpty()){
+            Registry.register(registry, id, level);
+        }
+        if (registry instanceof MappedRegistry<Level> mappedRegistry) {
+            mappedRegistry.freeze();
+        }
+
+        ((AccessorMinecraftServer)level.getServer()).getLevels().put(level.dimension(), level);
+        level.getServer().markWorldsDirty();
+        NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, false));
         TemporaryLevelManager.levels.add(level);
+        return level;
     }
 
     public static void unregisterLevel(TemporaryLevel level) {
         // idk how to deregister stuff
+        ((AccessorMinecraftServer)level.getServer()).getLevels().remove(level.dimension());
+        NeoForge.EVENT_BUS.post(new LevelEvent.Unload(level));
         ResourceLocation id = level.getId();
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, true));
+        level.getServer().markWorldsDirty();
         TemporaryLevelManager.levels.remove(level);
+    }
+
+    public static void deleteLevel(TemporaryLevel level) {
+    }
+
+    private static ChunkGenerator getRiftChunkGenerator(){
+        var biomeHolder = ServerLifecycleHooks.getCurrentServer().overworld().registryAccess().lookup(Registries.BIOME).flatMap(x -> x.get(Biomes.THE_VOID)).orElse(null);
+        if (biomeHolder == null) {
+            return null;
+        }
+        var flatSettings = new FlatLevelGeneratorSettings(Optional.empty(), biomeHolder, List.of());
+        List<FlatLayerInfo> layerInfos = new ArrayList<>();
+        layerInfos.add(new FlatLayerInfo(1, Blocks.STONE));
+        flatSettings = flatSettings.withBiomeAndLayers(layerInfos, Optional.empty(), biomeHolder);
+        return new FlatLevelSource(flatSettings);
     }
 }

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/mixin/AccessorMinecraftServer.java
@@ -1,0 +1,40 @@
+package com.wanderersoftherift.wotr.mixin;
+
+import net.minecraft.core.LayeredRegistryAccess;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.RegistryLayer;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.WorldData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+import java.util.concurrent.Executor;
+
+
+@Mixin(MinecraftServer.class)
+public interface AccessorMinecraftServer {
+    @Accessor
+    LayeredRegistryAccess<RegistryLayer> getRegistries();
+
+    @Accessor
+    Executor getExecutor();
+
+    @Accessor
+    LevelStorageSource.LevelStorageAccess getStorageSource();
+
+    @Accessor
+    WorldData getWorldData();
+
+    @Accessor
+    ChunkProgressListenerFactory getProgressListenerFactory();
+
+    @Accessor
+    Map<ResourceKey<Level>, ServerLevel> getLevels();
+}

src/main/java/com/wanderersoftherift/wotr/world/level/TemporaryLevel.java
@@ -1,19 +1,58 @@
 package com.wanderersoftherift.wotr.world.level;
 
+import com.wanderersoftherift.wotr.mixin.AccessorMinecraftServer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.world.RandomSequences;
+import net.minecraft.world.level.CustomSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.storage.DerivedLevelData;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.ServerLevelData;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+import java.util.List;
+import java.util.concurrent.Executor;
 
 public class TemporaryLevel extends ServerLevel {
     private final ResourceLocation id;
 
-    public TemporaryLevel(ResourceLocation id) {
-        super(
-                ServerLifecycleHooks.getCurrentServer(),
-                // ???
-        );
-
+    private TemporaryLevel(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess,
+                           ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener,
+                           boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime,
+                           RandomSequences randomSequences, ResourceLocation id) {
+        super(server, dispatcher, levelStorageAccess, serverLevelData, dimension, levelStem, progressListener, isDebug, biomeZoomSeed, customSpawners, tickTime, randomSequences);
         this.id = id;
+    }
+
+
+    public static TemporaryLevel create(ResourceLocation id, LevelStem stem) {
+        AccessorMinecraftServer server = (AccessorMinecraftServer) ServerLifecycleHooks.getCurrentServer();
+        var chunkProgressListener = server.getProgressListenerFactory().create(0);
+        var storageSource = server.getStorageSource();
+        var worldData = server.getWorldData();
+        var executor = server.getExecutor();
+
+        return new TemporaryLevel(
+            ServerLifecycleHooks.getCurrentServer(),
+            executor,
+            storageSource,
+            new DerivedLevelData(worldData, worldData.overworldData()),
+            ResourceKey.create(Registries.DIMENSION, id),
+            stem,
+            chunkProgressListener,
+            false,
+            0L,
+            List.of(),
+            false,
+            ServerLifecycleHooks.getCurrentServer().overworld().getRandomSequences(),
+            id
+        );
     }
 
     public ResourceLocation getId() {

src/main/java/com/wanderersoftherift/wotr/world/level/TemporaryLevelManager.java
@@ -2,11 +2,24 @@ package com.wanderersoftherift.wotr.world.level;
 
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import com.wanderersoftherift.wotr.network.S2CLevelListUpdatePacket;
+import com.wanderersoftherift.wotr.mixin.AccessorMinecraftServer;
+import net.minecraft.core.Holder;
+import net.minecraft.core.MappedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.levelgen.flat.FlatLayerInfo;
+import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,30 +29,80 @@ import java.util.UUID;
 public class TemporaryLevelManager {
     private static final List<TemporaryLevel> levels = new ArrayList<>();
 
+    // TODO: persistent dimensions (DO NOT SHUT DOWN THE SERVER IN RIFT - you won't be able to rejoin)
+    // TODO: deregister and delete files after exiting rift
     public static TemporaryLevel createRiftLevel() {
         ResourceLocation id = WanderersOfTheRift.id("rift_" + UUID.randomUUID());
 
-        TemporaryLevel level = new TemporaryLevel(id);
-        registerLevel(level);
-        return level;
-    }
-
-    private static void registerLevel(TemporaryLevel level) {
-        Optional<Registry<Level>> dimensionRegistry = level.registryAccess().lookup(Registries.DIMENSION);
-        if (dimensionRegistry.isEmpty()) {
-            return;
+        var ow = ServerLifecycleHooks.getCurrentServer().overworld();
+        Optional<Registry<LevelStem>> levelStemRegistry = ow.registryAccess().lookup(Registries.LEVEL_STEM);
+        if (levelStemRegistry.isEmpty()) {
+            return null;
         }
-        ResourceLocation id = level.getId();
+        Optional<Registry<Level>> dimensionRegistry = ow.registryAccess().lookup(Registries.DIMENSION);
+        if (dimensionRegistry.isEmpty()) {
+            return null;
+        }
+
+        ChunkGenerator chunkGen = getRiftChunkGenerator();
+        if (chunkGen == null) {
+            return null;
+        }
+
+        //TODO: figure out how to use the dimension type
+        var stem = new LevelStem(Holder.direct(ow.dimensionType()),chunkGen);
+        var stemRegistry = levelStemRegistry.get();
+        if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
+            mappedStemRegistry.unfreeze(false);
+        }
+        Registry.register(stemRegistry, id, stem);
+
+        if (stemRegistry instanceof MappedRegistry<LevelStem> mappedStemRegistry) {
+            mappedStemRegistry.freeze();
+        }
+
+        TemporaryLevel level = TemporaryLevel.create(id, stem);
+
         Registry<Level> registry = dimensionRegistry.get();
-        Registry.register(registry, id, level);
+        if (registry instanceof MappedRegistry<Level> mappedRegistry) {
+            mappedRegistry.unfreeze(false);
+        }
+        if (registry.get(id).isEmpty()){
+            Registry.register(registry, id, level);
+        }
+        if (registry instanceof MappedRegistry<Level> mappedRegistry) {
+            mappedRegistry.freeze();
+        }
+
+        ((AccessorMinecraftServer)level.getServer()).getLevels().put(level.dimension(), level);
+        level.getServer().markWorldsDirty();
+        NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, false));
         TemporaryLevelManager.levels.add(level);
+        return level;
     }
 
     public static void unregisterLevel(TemporaryLevel level) {
         // idk how to deregister stuff
+        ((AccessorMinecraftServer)level.getServer()).getLevels().remove(level.dimension());
+        NeoForge.EVENT_BUS.post(new LevelEvent.Unload(level));
         ResourceLocation id = level.getId();
         PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, true));
         TemporaryLevelManager.levels.remove(level);
+    }
+
+    public static void deleteLevel(TemporaryLevel level) {
+    }
+
+    private static ChunkGenerator getRiftChunkGenerator(){
+        var biomeHolder = ServerLifecycleHooks.getCurrentServer().overworld().registryAccess().lookup(Registries.BIOME).flatMap(x -> x.get(Biomes.THE_VOID)).orElse(null);
+        if (biomeHolder == null) {
+            return null;
+        }
+        var flatSettings = new FlatLevelGeneratorSettings(Optional.empty(), biomeHolder, List.of());
+        List<FlatLayerInfo> layerInfos = new ArrayList<>();
+        layerInfos.add(new FlatLayerInfo(1, Blocks.STONE));
+        flatSettings = flatSettings.withBiomeAndLayers(layerInfos, Optional.empty(), biomeHolder);
+        return new FlatLevelSource(flatSettings);
     }
 }