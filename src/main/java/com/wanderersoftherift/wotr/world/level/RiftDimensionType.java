Dimension-Delvers/dimension-delvers-mod

fix: register rift on startup to fix respawn crash

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/RiftDimensionType.java
@@ -1,37 +1,17 @@
 package com.dimensiondelvers.dimensiondelvers.world.level;
 
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.dimension.DimensionType;
 
-import java.util.OptionalLong;
-
 public class RiftDimensionType {
-    public static final DimensionType RIFT_DIMENSION;
     public static final RiftDimensionSpecialEffects RIFT_DIMENSION_RENDERER;
     public static final ResourceLocation RIFT_DIMENSION_RENDERER_KEY = DimensionDelvers.id("rift_dimension_renderer");
+    public static final ResourceKey<DimensionType> RIFT_DIMENSION_TYPE = ResourceKey.create(Registries.DIMENSION_TYPE, DimensionDelvers.id("rift_dimension"));
 
     static {
-        RIFT_DIMENSION = new DimensionType(
-                OptionalLong.of(0L),
-                false,
-                false,
-                false,
-                true,
-                1,
-                false,
-                false,
-                -64, // Minimum Y Level
-                64 + 320, // Height + Min Y = Max Y
-                64 + 320, // Logical Height
-                BlockTags.INFINIBURN_OVERWORLD,
-                RiftDimensionType.RIFT_DIMENSION_RENDERER_KEY,
-                0f,
-                new DimensionType.MonsterSettings(false, false, UniformInt.of(0, 0), 0) // unsure what values we want here
-        );
-
         RIFT_DIMENSION_RENDERER = new RiftDimensionSpecialEffects();
     }
 }

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/world/level/RiftDimensionType.java
@@ -1,37 +1,17 @@
 package com.wanderersoftherift.wotr.world.level;
 
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.dimension.DimensionType;
 
-import java.util.OptionalLong;
-
 public class RiftDimensionType {
-    public static final DimensionType RIFT_DIMENSION;
     public static final RiftDimensionSpecialEffects RIFT_DIMENSION_RENDERER;
     public static final ResourceLocation RIFT_DIMENSION_RENDERER_KEY = WanderersOfTheRift.id("rift_dimension_renderer");
+    public static final ResourceKey<DimensionType> RIFT_DIMENSION_TYPE = ResourceKey.create(Registries.DIMENSION_TYPE, WanderersOfTheRift.id("rift_dimension"));
 
     static {
-        RIFT_DIMENSION = new DimensionType(
-                OptionalLong.of(0L),
-                false,
-                false,
-                false,
-                true,
-                1,
-                false,
-                false,
-                -64, // Minimum Y Level
-                64 + 320, // Height + Min Y = Max Y
-                64 + 320, // Logical Height
-                BlockTags.INFINIBURN_OVERWORLD,
-                RiftDimensionType.RIFT_DIMENSION_RENDERER_KEY,
-                0f,
-                new DimensionType.MonsterSettings(false, false, UniformInt.of(0, 0), 0) // unsure what values we want here
-        );
-
         RIFT_DIMENSION_RENDERER = new RiftDimensionSpecialEffects();
     }
 }