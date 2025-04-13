Dimension-Delvers/dimension-delvers-mod

refactor: rename the client dimension registration packet

src/main/java/com/dimensiondelvers/dimensiondelvers/network/S2CLevelListUpdatePacket.java
@@ -14,27 +14,27 @@ import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.neoforge.network.handling.IPayloadHandler;
 import org.jetbrains.annotations.NotNull;
 
-public record S2CDimensionTypesUpdatePacket(
+public record S2CLevelListUpdatePacket(
         ResourceLocation resourceLocation,
         boolean removal
 ) implements CustomPacketPayload {
-    public static final CustomPacketPayload.Type<S2CDimensionTypesUpdatePacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(DimensionDelvers.MODID, "s2c_dimension_types_update"));
+    public static final CustomPacketPayload.Type<S2CLevelListUpdatePacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(DimensionDelvers.MODID, "s2c_dimension_types_update"));
 
-    public static final StreamCodec<ByteBuf, S2CDimensionTypesUpdatePacket> STREAM_CODEC = StreamCodec.composite(
+    public static final StreamCodec<ByteBuf, S2CLevelListUpdatePacket> STREAM_CODEC = StreamCodec.composite(
             ResourceLocation.STREAM_CODEC,
-            S2CDimensionTypesUpdatePacket::resourceLocation,
+            S2CLevelListUpdatePacket::resourceLocation,
             ByteBufCodecs.BOOL,
-            S2CDimensionTypesUpdatePacket::removal,
-            S2CDimensionTypesUpdatePacket::new
+            S2CLevelListUpdatePacket::removal,
+            S2CLevelListUpdatePacket::new
     );
 
     @Override
-    public CustomPacketPayload.@NotNull Type<S2CDimensionTypesUpdatePacket> type() {
+    public CustomPacketPayload.@NotNull Type<S2CLevelListUpdatePacket> type() {
         return TYPE;
     }
 
-    public static class S2CDimensionTypesUpdatePacketHandler implements IPayloadHandler<S2CDimensionTypesUpdatePacket> {
-        public void handle(@NotNull S2CDimensionTypesUpdatePacket packet, @NotNull IPayloadContext context) {
+    public static class S2CLevelListUpdatePacketHandler implements IPayloadHandler<S2CLevelListUpdatePacket> {
+        public void handle(@NotNull S2CLevelListUpdatePacket packet, @NotNull IPayloadContext context) {
             Player player = context.player();
             if (!(player instanceof LocalPlayer localPlayer)) {
                 return;

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/network/S2CLevelListUpdatePacket.java
@@ -14,27 +14,27 @@ import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.neoforge.network.handling.IPayloadHandler;
 import org.jetbrains.annotations.NotNull;
 
-public record S2CDimensionTypesUpdatePacket(
+public record S2CLevelListUpdatePacket(
         ResourceLocation resourceLocation,
         boolean removal
 ) implements CustomPacketPayload {
-    public static final CustomPacketPayload.Type<S2CDimensionTypesUpdatePacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(WanderersOfTheRift.MODID, "s2c_dimension_types_update"));
+    public static final CustomPacketPayload.Type<S2CLevelListUpdatePacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(WanderersOfTheRift.MODID, "s2c_dimension_types_update"));
 
-    public static final StreamCodec<ByteBuf, S2CDimensionTypesUpdatePacket> STREAM_CODEC = StreamCodec.composite(
+    public static final StreamCodec<ByteBuf, S2CLevelListUpdatePacket> STREAM_CODEC = StreamCodec.composite(
             ResourceLocation.STREAM_CODEC,
-            S2CDimensionTypesUpdatePacket::resourceLocation,
+            S2CLevelListUpdatePacket::resourceLocation,
             ByteBufCodecs.BOOL,
-            S2CDimensionTypesUpdatePacket::removal,
-            S2CDimensionTypesUpdatePacket::new
+            S2CLevelListUpdatePacket::removal,
+            S2CLevelListUpdatePacket::new
     );
 
     @Override
-    public CustomPacketPayload.@NotNull Type<S2CDimensionTypesUpdatePacket> type() {
+    public CustomPacketPayload.@NotNull Type<S2CLevelListUpdatePacket> type() {
         return TYPE;
     }
 
-    public static class S2CDimensionTypesUpdatePacketHandler implements IPayloadHandler<S2CDimensionTypesUpdatePacket> {
-        public void handle(@NotNull S2CDimensionTypesUpdatePacket packet, @NotNull IPayloadContext context) {
+    public static class S2CLevelListUpdatePacketHandler implements IPayloadHandler<S2CLevelListUpdatePacket> {
+        public void handle(@NotNull S2CLevelListUpdatePacket packet, @NotNull IPayloadContext context) {
             Player player = context.player();
             if (!(player instanceof LocalPlayer localPlayer)) {
                 return;