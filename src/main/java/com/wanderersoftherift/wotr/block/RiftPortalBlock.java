Dimension-Delvers/dimension-delvers-mod

refactor: simplify dynamic dimensions

src/main/java/com/dimensiondelvers/dimensiondelvers/block/RiftPortalBlock.java
@@ -1,11 +1,13 @@
 package com.dimensiondelvers.dimensiondelvers.block;
 
-import com.dimensiondelvers.dimensiondelvers.core.rift.RiftLevel;
+import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import com.dimensiondelvers.dimensiondelvers.core.rift.RiftData;
 import com.dimensiondelvers.dimensiondelvers.core.rift.RiftLevelManager;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
@@ -30,50 +32,42 @@ public class RiftPortalBlock extends Block {
 
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level,BlockPos pos, Player player, BlockHitResult hitResult) {
-        if (!level.isClientSide) {
-            if (level instanceof RiftLevel riftLevel) {
-                if (player instanceof ServerPlayer serverPlayer) {
-
-                    ResourceKey<Level> respawnKey = riftLevel.getPortalDimension();
-                    if (respawnKey == level.dimension()) {
-                        respawnKey = Level.OVERWORLD;
-                    }
-                    ServerLevel respawnDimension = level.getServer().getLevel(respawnKey);
-                    if (respawnDimension == null) {
-                        respawnDimension = level.getServer().overworld();
-                    }
-                    var respawnPos = riftLevel.getPortalPos().above();
-                    player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
-                    riftLevel.removePlayer(player.getUUID());
-                    RiftLevelManager.unregisterAndDeleteLevel(riftLevel);
-                } else {
-                    player.displayClientMessage(Component.literal("Failed to create rift"), true);
-                }
-
-                return InteractionResult.SUCCESS;
+        if (!level.isClientSide && player instanceof ServerPlayer serverPlayer && level instanceof ServerLevel serverLevel) {
+            if (RiftData.isRift(serverLevel)) {
+                return tpHome(serverPlayer, serverLevel);
             }
-            if (level.dimension().location().getNamespace().equals("dimensiondelvers")) {
-                if (player instanceof ServerPlayer serverPlayer) {
-                    player.displayClientMessage(Component.literal("You're in inactive rift - teleporting to spawn"), true);
-
-                    ServerLevel respawnDimension = level.getServer().overworld();
-                    var respawnPos = respawnDimension.getSharedSpawnPos();
-                    player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(),
-                        serverPlayer.getRespawnAngle(), 0, true);
-                }
-                return InteractionResult.SUCCESS;
-            }
-            RiftLevel lvl = RiftLevelManager.createRiftLevel(level.dimension(), pos);
-            if (lvl == null) {
-                player.displayClientMessage(Component.literal("Failed to create rift"), true);
-                return InteractionResult.FAIL;
-            }
-            lvl.addPlayer(player.getUUID());
-            player.displayClientMessage(Component.literal("Created rift with id: " + lvl.getId()), true);
-            player.teleportTo(lvl, 0.5, 0, 0.5, Set.of(), player.getYRot(), player.getXRot(), false);
-            NeoForge.EVENT_BUS.post(new PlayerEvent.PlayerChangedDimensionEvent(player, level.dimension(), lvl.dimension()));
-
+            return tpToRift(serverPlayer, serverLevel, pos);
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private static InteractionResult tpToRift(ServerPlayer player, ServerLevel level, BlockPos pos) {
+        ResourceLocation riftId = DimensionDelvers.id("rift_" + pos.getX() + "_" + pos.getY() + "_" + pos.getZ());
+        ServerLevel lvl = RiftLevelManager.getOrCreateRiftLevel(riftId, level.dimension(), pos);
+        if (lvl == null) {
+            player.displayClientMessage(Component.literal("Failed to create rift"), true);
+            return InteractionResult.FAIL;
+        }
+        RiftData.get(lvl).addPlayer(player.getUUID());
+        player.displayClientMessage(Component.literal("Created rift with id: " + lvl.dimension().location()), true);
+        player.teleportTo(lvl, 0.5, 0, 0.5, Set.of(), player.getYRot(), player.getXRot(), false);
+        NeoForge.EVENT_BUS.post(new PlayerEvent.PlayerChangedDimensionEvent(player, level.dimension(), lvl.dimension()));
+        return InteractionResult.SUCCESS;
+    }
+
+    private static InteractionResult tpHome(ServerPlayer serverPlayer, ServerLevel riftLevel) {
+            ResourceKey<Level> respawnKey = RiftData.get(riftLevel).getPortalDimension();
+            if (respawnKey == riftLevel.dimension()) {
+                respawnKey = Level.OVERWORLD;
+            }
+            ServerLevel respawnDimension = riftLevel.getServer().getLevel(respawnKey);
+            if (respawnDimension == null) {
+                respawnDimension = riftLevel.getServer().overworld();
+            }
+            var respawnPos = RiftData.get(riftLevel).getPortalPos().above();
+            serverPlayer.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
+            RiftData.get(riftLevel).removePlayer(serverPlayer.getUUID());
+            RiftLevelManager.unregisterAndDeleteLevel(riftLevel);
+            return InteractionResult.SUCCESS;
     }
 }

src/main/java/com/dimensiondelvers/dimensiondelvers/core/rift/RiftData.java
@@ -1,8 +1,11 @@
 package com.dimensiondelvers.dimensiondelvers.core.rift;
 
+import com.dimensiondelvers.dimensiondelvers.world.level.RiftDimensionType;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -10,7 +13,9 @@ import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.saveddata.SavedData;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -18,17 +23,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class RiftData extends SavedData {
-    private final ResourceKey<Level> portalDimension;
-    private final BlockPos portalPos;
+    private ResourceKey<Level> portalDimension;
+    private BlockPos portalPos;
     private final List<UUID> players;
 
 
-    public static SavedData.Factory<RiftData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
+    public static boolean isRift(ServerLevel level) {
+        Registry<DimensionType> dimTypes = level.registryAccess().lookupOrThrow(Registries.DIMENSION_TYPE);
+        Optional<Holder.Reference<DimensionType>> riftType = dimTypes.get(RiftDimensionType.RIFT_DIMENSION_TYPE);
+        return riftType.filter(dimensionTypeReference -> dimensionTypeReference.value() == level.dimensionType()).isPresent();
+    }
+
+    public static RiftData get(ServerLevel level) {
+        if (!isRift(level)) {
+            throw new IllegalArgumentException("Not a rift level");
+        }
+        return level.getDataStorage().computeIfAbsent(factory(level.getServer().overworld().dimension(), level.getServer().overworld().getSharedSpawnPos()), "rift_data");
+    }
+
+    private static SavedData.Factory<RiftData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
         return new SavedData.Factory<>(() -> new RiftData(portalDimension, portalPos, List.of()), RiftData::load);
     }
 
@@ -38,7 +57,7 @@ public class RiftData extends SavedData {
         this.players = new ArrayList<>(Objects.requireNonNull(players));
     }
 
-    public static RiftData load(CompoundTag tag, HolderLookup.Provider registries) {
+    private static RiftData load(CompoundTag tag, HolderLookup.Provider registries) {
         ResourceLocation portalDimensionLocation = ResourceLocation.parse(tag.getString("PortalDimension"));
         ResourceKey<Level> portalDimension = ResourceKey.create(Registries.DIMENSION, portalDimensionLocation);
         List<UUID> players = new ArrayList<>();
@@ -60,8 +79,18 @@ public class RiftData extends SavedData {
         return this.portalPos;
     }
 
+    public void setPortalPos(BlockPos portalPos) {
+        this.portalPos = portalPos;
+        this.setDirty();
+    }
+
     public ResourceKey<Level> getPortalDimension() {
         return this.portalDimension;
+    }
+
+    public void setPortalDimension(ResourceKey<Level> portalDimension) {
+        this.portalDimension = portalDimension;
+        this.setDirty();
     }
 
     public List<UUID> getPlayers() {

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/block/RiftPortalBlock.java
@@ -1,11 +1,13 @@
 package com.wanderersoftherift.wotr.block;
 
-import com.wanderersoftherift.wotr.core.rift.RiftLevel;
+import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import com.wanderersoftherift.wotr.core.rift.RiftLevelManager;
+import com.wanderersoftherift.wotr.core.rift.RiftData;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
@@ -30,50 +32,42 @@ public class RiftPortalBlock extends Block {
 
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level,BlockPos pos, Player player, BlockHitResult hitResult) {
-        if (!level.isClientSide) {
-            if (level instanceof RiftLevel riftLevel) {
-                if (player instanceof ServerPlayer serverPlayer) {
-
-                    ResourceKey<Level> respawnKey = riftLevel.getPortalDimension();
-                    if (respawnKey == level.dimension()) {
-                        respawnKey = Level.OVERWORLD;
-                    }
-                    ServerLevel respawnDimension = level.getServer().getLevel(respawnKey);
-                    if (respawnDimension == null) {
-                        respawnDimension = level.getServer().overworld();
-                    }
-                    var respawnPos = riftLevel.getPortalPos().above();
-                    player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
-                    riftLevel.removePlayer(player.getUUID());
-                    RiftLevelManager.unregisterAndDeleteLevel(riftLevel);
-                } else {
-                    player.displayClientMessage(Component.literal("Failed to create rift"), true);
-                }
-
-                return InteractionResult.SUCCESS;
+        if (!level.isClientSide && player instanceof ServerPlayer serverPlayer && level instanceof ServerLevel serverLevel) {
+            if (RiftData.isRift(serverLevel)) {
+                return tpHome(serverPlayer, serverLevel);
             }
-            if (level.dimension().location().getNamespace().equals("wotr")) {
-                if (player instanceof ServerPlayer serverPlayer) {
-                    player.displayClientMessage(Component.literal("You're in inactive rift - teleporting to spawn"), true);
-
-                    ServerLevel respawnDimension = level.getServer().overworld();
-                    var respawnPos = respawnDimension.getSharedSpawnPos();
-                    player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(),
-                        serverPlayer.getRespawnAngle(), 0, true);
-                }
-                return InteractionResult.SUCCESS;
-            }
-            RiftLevel lvl = RiftLevelManager.createRiftLevel(level.dimension(), pos);
-            if (lvl == null) {
-                player.displayClientMessage(Component.literal("Failed to create rift"), true);
-                return InteractionResult.FAIL;
-            }
-            lvl.addPlayer(player.getUUID());
-            player.displayClientMessage(Component.literal("Created rift with id: " + lvl.getId()), true);
-            player.teleportTo(lvl, 0.5, 0, 0.5, Set.of(), player.getYRot(), player.getXRot(), false);
-            NeoForge.EVENT_BUS.post(new PlayerEvent.PlayerChangedDimensionEvent(player, level.dimension(), lvl.dimension()));
-
+            return tpToRift(serverPlayer, serverLevel, pos);
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private static InteractionResult tpToRift(ServerPlayer player, ServerLevel level, BlockPos pos) {
+        ResourceLocation riftId = WanderersOfTheRift.id("rift_" + pos.getX() + "_" + pos.getY() + "_" + pos.getZ());
+        ServerLevel lvl = RiftLevelManager.getOrCreateRiftLevel(riftId, level.dimension(), pos);
+        if (lvl == null) {
+            player.displayClientMessage(Component.literal("Failed to create rift"), true);
+            return InteractionResult.FAIL;
+        }
+        RiftData.get(lvl).addPlayer(player.getUUID());
+        player.displayClientMessage(Component.literal("Created rift with id: " + lvl.dimension().location()), true);
+        player.teleportTo(lvl, 0.5, 0, 0.5, Set.of(), player.getYRot(), player.getXRot(), false);
+        NeoForge.EVENT_BUS.post(new PlayerEvent.PlayerChangedDimensionEvent(player, level.dimension(), lvl.dimension()));
+        return InteractionResult.SUCCESS;
+    }
+
+    private static InteractionResult tpHome(ServerPlayer serverPlayer, ServerLevel riftLevel) {
+            ResourceKey<Level> respawnKey = RiftData.get(riftLevel).getPortalDimension();
+            if (respawnKey == riftLevel.dimension()) {
+                respawnKey = Level.OVERWORLD;
+            }
+            ServerLevel respawnDimension = riftLevel.getServer().getLevel(respawnKey);
+            if (respawnDimension == null) {
+                respawnDimension = riftLevel.getServer().overworld();
+            }
+            var respawnPos = RiftData.get(riftLevel).getPortalPos().above();
+            serverPlayer.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
+            RiftData.get(riftLevel).removePlayer(serverPlayer.getUUID());
+            RiftLevelManager.unregisterAndDeleteLevel(riftLevel);
+            return InteractionResult.SUCCESS;
     }
 }

src/main/java/com/wanderersoftherift/wotr/core/rift/RiftData.java
@@ -1,8 +1,11 @@
 package com.wanderersoftherift.wotr.core.rift;
 
+import com.wanderersoftherift.wotr.world.level.RiftDimensionType;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -10,7 +13,9 @@ import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.saveddata.SavedData;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -18,17 +23,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class RiftData extends SavedData {
-    private final ResourceKey<Level> portalDimension;
-    private final BlockPos portalPos;
+    private ResourceKey<Level> portalDimension;
+    private BlockPos portalPos;
     private final List<UUID> players;
 
 
-    public static SavedData.Factory<RiftData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
+    public static boolean isRift(ServerLevel level) {
+        Registry<DimensionType> dimTypes = level.registryAccess().lookupOrThrow(Registries.DIMENSION_TYPE);
+        Optional<Holder.Reference<DimensionType>> riftType = dimTypes.get(RiftDimensionType.RIFT_DIMENSION_TYPE);
+        return riftType.filter(dimensionTypeReference -> dimensionTypeReference.value() == level.dimensionType()).isPresent();
+    }
+
+    public static RiftData get(ServerLevel level) {
+        if (!isRift(level)) {
+            throw new IllegalArgumentException("Not a rift level");
+        }
+        return level.getDataStorage().computeIfAbsent(factory(level.getServer().overworld().dimension(), level.getServer().overworld().getSharedSpawnPos()), "rift_data");
+    }
+
+    private static SavedData.Factory<RiftData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
         return new SavedData.Factory<>(() -> new RiftData(portalDimension, portalPos, List.of()), RiftData::load);
     }
 
@@ -38,7 +57,7 @@ public class RiftData extends SavedData {
         this.players = new ArrayList<>(Objects.requireNonNull(players));
     }
 
-    public static RiftData load(CompoundTag tag, HolderLookup.Provider registries) {
+    private static RiftData load(CompoundTag tag, HolderLookup.Provider registries) {
         ResourceLocation portalDimensionLocation = ResourceLocation.parse(tag.getString("PortalDimension"));
         ResourceKey<Level> portalDimension = ResourceKey.create(Registries.DIMENSION, portalDimensionLocation);
         List<UUID> players = new ArrayList<>();
@@ -60,8 +79,18 @@ public class RiftData extends SavedData {
         return this.portalPos;
     }
 
+    public void setPortalPos(BlockPos portalPos) {
+        this.portalPos = portalPos;
+        this.setDirty();
+    }
+
     public ResourceKey<Level> getPortalDimension() {
         return this.portalDimension;
+    }
+
+    public void setPortalDimension(ResourceKey<Level> portalDimension) {
+        this.portalDimension = portalDimension;
+        this.setDirty();
     }
 
     public List<UUID> getPlayers() {