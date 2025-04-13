Dimension-Delvers/dimension-delvers-mod

refactor: rename rift related classes and move their registration to core pkg

src/main/java/com/dimensiondelvers/dimensiondelvers/block/RiftPortalBlock.java
@@ -1,7 +1,7 @@
 package com.dimensiondelvers.dimensiondelvers.block;
 
-import com.dimensiondelvers.dimensiondelvers.world.level.TemporaryLevel;
-import com.dimensiondelvers.dimensiondelvers.world.level.TemporaryLevelManager;
+import com.dimensiondelvers.dimensiondelvers.core.rift.RiftLevel;
+import com.dimensiondelvers.dimensiondelvers.core.rift.RiftLevelManager;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -31,10 +31,10 @@ public class RiftPortalBlock extends Block {
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level,BlockPos pos, Player player, BlockHitResult hitResult) {
         if (!level.isClientSide) {
-            if (level instanceof TemporaryLevel temporaryLevel) {
+            if (level instanceof RiftLevel riftLevel) {
                 if (player instanceof ServerPlayer serverPlayer) {
 
-                    ResourceKey<Level> respawnKey = temporaryLevel.getPortalDimension();
+                    ResourceKey<Level> respawnKey = riftLevel.getPortalDimension();
                     if (respawnKey == level.dimension()) {
                         respawnKey = Level.OVERWORLD;
                     }
@@ -42,10 +42,10 @@ public class RiftPortalBlock extends Block {
                     if (respawnDimension == null) {
                         respawnDimension = level.getServer().overworld();
                     }
-                    var respawnPos = temporaryLevel.getPortalPos().above();
+                    var respawnPos = riftLevel.getPortalPos().above();
                     player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
-                    temporaryLevel.removePlayer(player.getUUID());
-                    TemporaryLevelManager.unregisterAndDeleteLevel(temporaryLevel);
+                    riftLevel.removePlayer(player.getUUID());
+                    RiftLevelManager.unregisterAndDeleteLevel(riftLevel);
                 } else {
                     player.displayClientMessage(Component.literal("Failed to create rift"), true);
                 }
@@ -63,7 +63,7 @@ public class RiftPortalBlock extends Block {
                 }
                 return InteractionResult.SUCCESS;
             }
-            TemporaryLevel lvl = TemporaryLevelManager.createRiftLevel(level.dimension(), pos);
+            RiftLevel lvl = RiftLevelManager.createRiftLevel(level.dimension(), pos);
             if (lvl == null) {
                 player.displayClientMessage(Component.literal("Failed to create rift"), true);
                 return InteractionResult.FAIL;

src/main/java/com/dimensiondelvers/dimensiondelvers/core/rift/RiftData.java
@@ -1,4 +1,4 @@
-package com.dimensiondelvers.dimensiondelvers.world.level;
+package com.dimensiondelvers.dimensiondelvers.core.rift;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
@@ -22,28 +22,28 @@ import java.util.UUID;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class RiftSavedData extends SavedData {
+public class RiftData extends SavedData {
     private final ResourceKey<Level> portalDimension;
     private final BlockPos portalPos;
     private final List<UUID> players;
 
 
-    public static SavedData.Factory<RiftSavedData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
-        return new SavedData.Factory<>(() -> new RiftSavedData(portalDimension, portalPos, List.of()), RiftSavedData::load);
+    public static SavedData.Factory<RiftData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
+        return new SavedData.Factory<>(() -> new RiftData(portalDimension, portalPos, List.of()), RiftData::load);
     }
 
-    private RiftSavedData(ResourceKey<Level> portalDimension, BlockPos portalPos, List<UUID> players) {
+    private RiftData(ResourceKey<Level> portalDimension, BlockPos portalPos, List<UUID> players) {
         this.portalDimension = Objects.requireNonNull(portalDimension);
         this.portalPos = Objects.requireNonNull(portalPos);
         this.players = new ArrayList<>(Objects.requireNonNull(players));
     }
 
-    public static RiftSavedData load(CompoundTag tag, HolderLookup.Provider registries) {
+    public static RiftData load(CompoundTag tag, HolderLookup.Provider registries) {
         ResourceLocation portalDimensionLocation = ResourceLocation.parse(tag.getString("PortalDimension"));
         ResourceKey<Level> portalDimension = ResourceKey.create(Registries.DIMENSION, portalDimensionLocation);
         List<UUID> players = new ArrayList<>();
         tag.getList("Players", Tag.TAG_STRING).forEach( player -> players.add(UUID.fromString(player.getAsString())));
-        return new RiftSavedData(portalDimension, BlockPos.of(tag.getLong("PortalPos")), players);
+        return new RiftData(portalDimension, BlockPos.of(tag.getLong("PortalPos")), players);
     }
 
     @Override

src/main/java/com/dimensiondelvers/dimensiondelvers/core/rift/RiftLevel.java
@@ -1,4 +1,4 @@
-package com.dimensiondelvers.dimensiondelvers.world.level;
+package com.dimensiondelvers.dimensiondelvers.core.rift;
 
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
 import com.dimensiondelvers.dimensiondelvers.mixin.AccessorMinecraftServer;
@@ -22,21 +22,21 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 
-public class TemporaryLevel extends ServerLevel {
+public class RiftLevel extends ServerLevel {
     private final ResourceLocation id;
-    private final RiftSavedData data;
+    private final RiftData data;
 
-    private TemporaryLevel(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess,
-                           ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener,
-                           boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime,
-                           RandomSequences randomSequences, ResourceLocation id, ResourceKey<Level> portalDimension, BlockPos portalPos) {
+    private RiftLevel(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess,
+                      ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener,
+                      boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime,
+                      RandomSequences randomSequences, ResourceLocation id, ResourceKey<Level> portalDimension, BlockPos portalPos) {
         super(server, dispatcher, levelStorageAccess, serverLevelData, dimension, levelStem, progressListener, isDebug, biomeZoomSeed, customSpawners, tickTime, randomSequences);
         this.id = id;
-        this.data = this.getDataStorage().computeIfAbsent(RiftSavedData.factory(portalDimension, portalPos), "data_"+ id.getPath());
+        this.data = this.getDataStorage().computeIfAbsent(RiftData.factory(portalDimension, portalPos), "data_"+ id.getPath());
     }
 
 
-    public static TemporaryLevel create(ResourceLocation id, LevelStem stem, ResourceKey<Level> portalDimension, BlockPos portalPos) {
+    public static RiftLevel create(ResourceLocation id, LevelStem stem, ResourceKey<Level> portalDimension, BlockPos portalPos) {
         AccessorMinecraftServer server = (AccessorMinecraftServer) ServerLifecycleHooks.getCurrentServer();
         var chunkProgressListener = server.getProgressListenerFactory().create(0);
         var storageSource = server.getStorageSource();
@@ -44,12 +44,12 @@ public class TemporaryLevel extends ServerLevel {
         var executor = server.getExecutor();
 
         if (portalDimension == null || portalPos == null) {
-            DimensionDelvers.LOGGER.warn("Tried to create rift {} with portal from dimension {} at position {}, using overworld spawnpoint instead.",id, portalDimension, portalPos);
+            DimensionDelvers.LOGGER.warn("Tried to create rift {} with portal from dimension {} at position {}, using overworld spawnpoint instead.", id, portalDimension, portalPos);
             portalDimension = Level.OVERWORLD;
             portalPos = ServerLifecycleHooks.getCurrentServer().overworld().getSharedSpawnPos();
         }
 
-        return new TemporaryLevel(
+        return new RiftLevel(
             ServerLifecycleHooks.getCurrentServer(),
             executor,
             storageSource,

src/main/java/com/dimensiondelvers/dimensiondelvers/mixin/MixinMinecraftServer.java
@@ -1,6 +1,8 @@
 package com.dimensiondelvers.dimensiondelvers.mixin;
 
-import com.dimensiondelvers.dimensiondelvers.world.level.TemporaryLevel;
+import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import com.dimensiondelvers.dimensiondelvers.core.rift.RiftLevel;
+import com.dimensiondelvers.dimensiondelvers.core.rift.RiftLevelManager;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.Registry;
@@ -28,7 +30,7 @@ public abstract class MixinMinecraftServer {
 
     // This will be injected when overworld is already loaded, but nothing else is
     // It will split the dimension set into 2 - rifts and nonRifts
-    // It will then create TemporaryLevel for each rift and load it
+    // It will then create RiftLevel for each rift and load it
     // nonRifts will be passed back for normal loading
     @WrapOperation(method = "createLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;entrySet()Ljava/util/Set;"))
     private Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>> createRift(Registry<LevelStem> instance, Operation<Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>>> original) {
@@ -36,9 +38,11 @@ public abstract class MixinMinecraftServer {
         var rifts = originalSet.stream().filter(e -> e.getKey().location().getNamespace().equals("dimensiondelvers")).toList();
         var nonRifts = originalSet.stream().filter(e -> !e.getKey().location().getNamespace().equals("dimensiondelvers")).collect(Collectors.toSet());
         for (Map.Entry<ResourceKey<LevelStem>, LevelStem> rift : rifts) {
-            var level = TemporaryLevel.create(rift.getKey().location(), rift.getValue(), null, null);
+            var level = RiftLevel.create(rift.getKey().location(), rift.getValue(), null, null);
             this.forgeGetWorldMap().put(ResourceKey.create(Registries.DIMENSION, rift.getKey().location()), level);
+            RiftLevelManager.addActiveRift(level);
             NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
+            DimensionDelvers.LOGGER.debug("Loaded rift level {}", rift.getKey().location());
         }
         return nonRifts;
     }

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/block/RiftPortalBlock.java
@@ -1,7 +1,7 @@
 package com.wanderersoftherift.wotr.block;
 
-import com.wanderersoftherift.wotr.world.level.TemporaryLevel;
-import com.wanderersoftherift.wotr.world.level.TemporaryLevelManager;
+import com.wanderersoftherift.wotr.core.rift.RiftLevel;
+import com.wanderersoftherift.wotr.core.rift.RiftLevelManager;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -31,10 +31,10 @@ public class RiftPortalBlock extends Block {
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level,BlockPos pos, Player player, BlockHitResult hitResult) {
         if (!level.isClientSide) {
-            if (level instanceof TemporaryLevel temporaryLevel) {
+            if (level instanceof RiftLevel riftLevel) {
                 if (player instanceof ServerPlayer serverPlayer) {
 
-                    ResourceKey<Level> respawnKey = temporaryLevel.getPortalDimension();
+                    ResourceKey<Level> respawnKey = riftLevel.getPortalDimension();
                     if (respawnKey == level.dimension()) {
                         respawnKey = Level.OVERWORLD;
                     }
@@ -42,10 +42,10 @@ public class RiftPortalBlock extends Block {
                     if (respawnDimension == null) {
                         respawnDimension = level.getServer().overworld();
                     }
-                    var respawnPos = temporaryLevel.getPortalPos().above();
+                    var respawnPos = riftLevel.getPortalPos().above();
                     player.teleportTo(respawnDimension, respawnPos.getCenter().x(), respawnPos.getY(), respawnPos.getCenter().z(), Set.of(), serverPlayer.getRespawnAngle(), 0, true);
-                    temporaryLevel.removePlayer(player.getUUID());
-                    TemporaryLevelManager.unregisterAndDeleteLevel(temporaryLevel);
+                    riftLevel.removePlayer(player.getUUID());
+                    RiftLevelManager.unregisterAndDeleteLevel(riftLevel);
                 } else {
                     player.displayClientMessage(Component.literal("Failed to create rift"), true);
                 }
@@ -63,7 +63,7 @@ public class RiftPortalBlock extends Block {
                 }
                 return InteractionResult.SUCCESS;
             }
-            TemporaryLevel lvl = TemporaryLevelManager.createRiftLevel(level.dimension(), pos);
+            RiftLevel lvl = RiftLevelManager.createRiftLevel(level.dimension(), pos);
             if (lvl == null) {
                 player.displayClientMessage(Component.literal("Failed to create rift"), true);
                 return InteractionResult.FAIL;

src/main/java/com/wanderersoftherift/wotr/core/rift/RiftData.java
@@ -1,4 +1,4 @@
-package com.wanderersoftherift.wotr.world.level;
+package com.wanderersoftherift.wotr.core.rift;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
@@ -22,28 +22,28 @@ import java.util.UUID;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class RiftSavedData extends SavedData {
+public class RiftData extends SavedData {
     private final ResourceKey<Level> portalDimension;
     private final BlockPos portalPos;
     private final List<UUID> players;
 
 
-    public static SavedData.Factory<RiftSavedData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
-        return new SavedData.Factory<>(() -> new RiftSavedData(portalDimension, portalPos, List.of()), RiftSavedData::load);
+    public static SavedData.Factory<RiftData> factory(ResourceKey<Level> portalDimension, BlockPos portalPos) {
+        return new SavedData.Factory<>(() -> new RiftData(portalDimension, portalPos, List.of()), RiftData::load);
     }
 
-    private RiftSavedData(ResourceKey<Level> portalDimension, BlockPos portalPos, List<UUID> players) {
+    private RiftData(ResourceKey<Level> portalDimension, BlockPos portalPos, List<UUID> players) {
         this.portalDimension = Objects.requireNonNull(portalDimension);
         this.portalPos = Objects.requireNonNull(portalPos);
         this.players = new ArrayList<>(Objects.requireNonNull(players));
     }
 
-    public static RiftSavedData load(CompoundTag tag, HolderLookup.Provider registries) {
+    public static RiftData load(CompoundTag tag, HolderLookup.Provider registries) {
         ResourceLocation portalDimensionLocation = ResourceLocation.parse(tag.getString("PortalDimension"));
         ResourceKey<Level> portalDimension = ResourceKey.create(Registries.DIMENSION, portalDimensionLocation);
         List<UUID> players = new ArrayList<>();
         tag.getList("Players", Tag.TAG_STRING).forEach( player -> players.add(UUID.fromString(player.getAsString())));
-        return new RiftSavedData(portalDimension, BlockPos.of(tag.getLong("PortalPos")), players);
+        return new RiftData(portalDimension, BlockPos.of(tag.getLong("PortalPos")), players);
     }
 
     @Override

src/main/java/com/wanderersoftherift/wotr/core/rift/RiftLevel.java
@@ -1,4 +1,4 @@
-package com.wanderersoftherift.wotr.world.level;
+package com.wanderersoftherift.wotr.core.rift;
 
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import com.wanderersoftherift.wotr.mixin.AccessorMinecraftServer;
@@ -22,21 +22,21 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 
-public class TemporaryLevel extends ServerLevel {
+public class RiftLevel extends ServerLevel {
     private final ResourceLocation id;
-    private final RiftSavedData data;
+    private final RiftData data;
 
-    private TemporaryLevel(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess,
-                           ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener,
-                           boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime,
-                           RandomSequences randomSequences, ResourceLocation id, ResourceKey<Level> portalDimension, BlockPos portalPos) {
+    private RiftLevel(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess,
+                      ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener,
+                      boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime,
+                      RandomSequences randomSequences, ResourceLocation id, ResourceKey<Level> portalDimension, BlockPos portalPos) {
         super(server, dispatcher, levelStorageAccess, serverLevelData, dimension, levelStem, progressListener, isDebug, biomeZoomSeed, customSpawners, tickTime, randomSequences);
         this.id = id;
-        this.data = this.getDataStorage().computeIfAbsent(RiftSavedData.factory(portalDimension, portalPos), "data_"+ id.getPath());
+        this.data = this.getDataStorage().computeIfAbsent(RiftData.factory(portalDimension, portalPos), "data_"+ id.getPath());
     }
 
 
-    public static TemporaryLevel create(ResourceLocation id, LevelStem stem, ResourceKey<Level> portalDimension, BlockPos portalPos) {
+    public static RiftLevel create(ResourceLocation id, LevelStem stem, ResourceKey<Level> portalDimension, BlockPos portalPos) {
         AccessorMinecraftServer server = (AccessorMinecraftServer) ServerLifecycleHooks.getCurrentServer();
         var chunkProgressListener = server.getProgressListenerFactory().create(0);
         var storageSource = server.getStorageSource();
@@ -49,7 +49,7 @@ public class TemporaryLevel extends ServerLevel {
             portalPos = ServerLifecycleHooks.getCurrentServer().overworld().getSharedSpawnPos();
         }
 
-        return new TemporaryLevel(
+        return new RiftLevel(
             ServerLifecycleHooks.getCurrentServer(),
             executor,
             storageSource,

src/main/java/com/wanderersoftherift/wotr/mixin/MixinMinecraftServer.java
@@ -1,6 +1,8 @@
 package com.wanderersoftherift.wotr.mixin;
 
-import com.wanderersoftherift.wotr.world.level.TemporaryLevel;
+import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import com.wanderersoftherift.wotr.core.rift.RiftLevel;
+import com.wanderersoftherift.wotr.core.rift.RiftLevelManager;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.Registry;
@@ -28,7 +30,7 @@ public abstract class MixinMinecraftServer {
 
     // This will be injected when overworld is already loaded, but nothing else is
     // It will split the dimension set into 2 - rifts and nonRifts
-    // It will then create TemporaryLevel for each rift and load it
+    // It will then create RiftLevel for each rift and load it
     // nonRifts will be passed back for normal loading
     @WrapOperation(method = "createLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;entrySet()Ljava/util/Set;"))
     private Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>> createRift(Registry<LevelStem> instance, Operation<Set<Map.Entry<ResourceKey<LevelStem>, LevelStem>>> original) {
@@ -36,9 +38,11 @@ public abstract class MixinMinecraftServer {
         var rifts = originalSet.stream().filter(e -> e.getKey().location().getNamespace().equals("wotr")).toList();
         var nonRifts = originalSet.stream().filter(e -> !e.getKey().location().getNamespace().equals("wotr")).collect(Collectors.toSet());
         for (Map.Entry<ResourceKey<LevelStem>, LevelStem> rift : rifts) {
-            var level = TemporaryLevel.create(rift.getKey().location(), rift.getValue(), null, null);
+            var level = RiftLevel.create(rift.getKey().location(), rift.getValue(), null, null);
             this.forgeGetWorldMap().put(ResourceKey.create(Registries.DIMENSION, rift.getKey().location()), level);
+            RiftLevelManager.addActiveRift(level);
             NeoForge.EVENT_BUS.post(new LevelEvent.Load(level));
+            DimensionDelvers.LOGGER.debug("Loaded rift level {}", rift.getKey().location());
         }
         return nonRifts;
     }