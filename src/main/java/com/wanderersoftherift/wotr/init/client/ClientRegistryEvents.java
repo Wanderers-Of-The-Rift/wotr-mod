Dimension-Delvers/dimension-delvers-mod

fix: don't load client only classes on dedicated server

src/main/java/com/dimensiondelvers/dimensiondelvers/events/client/ClientRegistryEvents.java
@@ -3,11 +3,14 @@ package com.dimensiondelvers.dimensiondelvers.events.client;
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
 import com.dimensiondelvers.dimensiondelvers.client.render.item.properties.select.SelectRuneGemShape;
 import com.dimensiondelvers.dimensiondelvers.client.tooltip.ImageTooltipRenderer;
+import com.dimensiondelvers.dimensiondelvers.world.level.RiftDimensionSpecialEffects;
+import com.dimensiondelvers.dimensiondelvers.world.level.RiftDimensionType;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
+import net.neoforged.neoforge.client.event.RegisterDimensionSpecialEffectsEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 
 @EventBusSubscriber(modid = DimensionDelvers.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
@@ -21,5 +24,10 @@ public class ClientRegistryEvents {
     @SubscribeEvent
     public static void registerClientTooltipComponents(RegisterClientTooltipComponentFactoriesEvent event) {
         event.register(ImageTooltipRenderer.ImageComponent.class, ImageTooltipRenderer::new);
+    }
+
+    @SubscribeEvent
+    private static void registerClientDimensionEffects(RegisterDimensionSpecialEffectsEvent event) {
+        event.register(RiftDimensionType.RIFT_DIMENSION_RENDERER_KEY, new RiftDimensionSpecialEffects());
     }
 }

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/init/client/ClientRegistryEvents.java
@@ -18,6 +18,8 @@ import com.wanderersoftherift.wotr.gui.screen.KeyForgeScreen;
 import com.wanderersoftherift.wotr.gui.screen.RuneAnvilScreen;
 import com.wanderersoftherift.wotr.init.ModEntityTypes;
 import com.wanderersoftherift.wotr.init.ModMenuTypes;
+import com.wanderersoftherift.wotr.world.level.RiftDimensionSpecialEffects;
+import com.wanderersoftherift.wotr.world.level.RiftDimensionType;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -113,5 +115,10 @@ public final class ClientRegistryEvents {
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event) {
         event.register(JIGSAW_NAME_TOGGLE_KEY);
+    }
+
+    @SubscribeEvent
+    private static void registerClientDimensionEffects(RegisterDimensionSpecialEffectsEvent event) {
+        event.register(RiftDimensionType.RIFT_DIMENSION_RENDERER_KEY, new RiftDimensionSpecialEffects());
     }
 }