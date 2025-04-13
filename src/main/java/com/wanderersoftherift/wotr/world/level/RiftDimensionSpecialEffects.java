Dimension-Delvers/dimension-delvers-mod

feat(wip): start making a temporarylevel system, unsure how to continue

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/RiftDimensionSpecialEffects.java
@@ -1,0 +1,50 @@
+package com.dimensiondelvers.dimensiondelvers.world.level;
+
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+
+public class RiftDimensionSpecialEffects extends DimensionSpecialEffects {
+    public RiftDimensionSpecialEffects() {
+        super(
+                0.0f,
+                false,
+                DimensionSpecialEffects.SkyType.NONE,
+                true, // idk, might want to change this no clue what it does tbh
+                true // this too
+        );
+    }
+
+    @Override
+    public @NotNull Vec3 getBrightnessDependentFogColor(@NotNull Vec3 vec3, float v) {
+        return new Vec3(0, 0, 0);
+    }
+
+    @Override
+    public boolean isFoggyAt(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean renderClouds(@NotNull ClientLevel level, int ticks, float partialTick, double camX, double camY, double camZ, @NotNull Matrix4f modelViewMatrix, @NotNull Matrix4f projectionMatrix) {
+        return false;
+    }
+
+    @Override
+    public boolean renderSky(@NotNull ClientLevel level, int ticks, float partialTick, @NotNull Matrix4f modelViewMatrix, @NotNull Camera camera, @NotNull Matrix4f projectionMatrix, @NotNull Runnable setupFog) {
+        return false;
+    }
+
+    @Override
+    public boolean renderSnowAndRain(@NotNull ClientLevel level, int ticks, float partialTick, double camX, double camY, double camZ) {
+        return false;
+    }
+
+    @Override
+    public boolean tickRain(@NotNull ClientLevel level, int ticks, @NotNull Camera camera) {
+        return false;
+    }
+}

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/RiftDimensionType.java
@@ -1,0 +1,37 @@
+package com.dimensiondelvers.dimensiondelvers.world.level;
+
+import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.dimension.DimensionType;
+
+import java.util.OptionalLong;
+
+public class RiftDimensionType {
+    public static final DimensionType RIFT_DIMENSION;
+    public static final RiftDimensionSpecialEffects RIFT_DIMENSION_RENDERER;
+    public static final ResourceLocation RIFT_DIMENSION_RENDERER_KEY = DimensionDelvers.id("rift_dimension_renderer");
+
+    static {
+        RIFT_DIMENSION = new DimensionType(
+                OptionalLong.of(0L),
+                false,
+                false,
+                false,
+                true,
+                1,
+                false,
+                false,
+                -64, // Minimum Y Level
+                64 + 320, // Height + Min Y = Max Y
+                64 + 320, // Logical Height
+                BlockTags.INFINIBURN_OVERWORLD,
+                RiftDimensionType.RIFT_DIMENSION_RENDERER_KEY,
+                0f,
+                new DimensionType.MonsterSettings(false, false, UniformInt.of(0, 0), 0) // unsure what values we want here
+        );
+
+        RIFT_DIMENSION_RENDERER = new RiftDimensionSpecialEffects();
+    }
+}

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/TemporaryLevel.java
@@ -1,0 +1,22 @@
+package com.dimensiondelvers.dimensiondelvers.world.level;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+public class TemporaryLevel extends ServerLevel {
+    private final ResourceLocation id;
+
+    public TemporaryLevel(ResourceLocation id) {
+        super(
+                ServerLifecycleHooks.getCurrentServer(),
+                // ???
+        );
+
+        this.id = id;
+    }
+
+    public ResourceLocation getId() {
+        return id;
+    }
+}

src/main/java/com/dimensiondelvers/dimensiondelvers/world/level/TemporaryLevelManager.java
@@ -1,0 +1,45 @@
+package com.dimensiondelvers.dimensiondelvers.world.level;
+
+import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
+import com.dimensiondelvers.dimensiondelvers.network.S2CLevelListUpdatePacket;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class TemporaryLevelManager {
+    private static final List<TemporaryLevel> levels = new ArrayList<>();
+
+    public static TemporaryLevel createRiftLevel() {
+        ResourceLocation id = DimensionDelvers.id("rift_" + UUID.randomUUID());
+
+        TemporaryLevel level = new TemporaryLevel(id);
+        registerLevel(level);
+        return level;
+    }
+
+    private static void registerLevel(TemporaryLevel level) {
+        Optional<Registry<Level>> dimensionRegistry = level.registryAccess().lookup(Registries.DIMENSION);
+        if (dimensionRegistry.isEmpty()) {
+            return;
+        }
+        ResourceLocation id = level.getId();
+        Registry<Level> registry = dimensionRegistry.get();
+        Registry.register(registry, id, level);
+        PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, false));
+        TemporaryLevelManager.levels.add(level);
+    }
+
+    public static void unregisterLevel(TemporaryLevel level) {
+        // idk how to deregister stuff
+        ResourceLocation id = level.getId();
+        PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, true));
+        TemporaryLevelManager.levels.remove(level);
+    }
+}

Wanderers-Of-The-Rift/wotr-mod

src/main/java/com/wanderersoftherift/wotr/world/level/RiftDimensionSpecialEffects.java
@@ -1,0 +1,50 @@
+package com.wanderersoftherift.wotr.world.level;
+
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+
+public class RiftDimensionSpecialEffects extends DimensionSpecialEffects {
+    public RiftDimensionSpecialEffects() {
+        super(
+                0.0f,
+                false,
+                DimensionSpecialEffects.SkyType.NONE,
+                true, // idk, might want to change this no clue what it does tbh
+                true // this too
+        );
+    }
+
+    @Override
+    public @NotNull Vec3 getBrightnessDependentFogColor(@NotNull Vec3 vec3, float v) {
+        return new Vec3(0, 0, 0);
+    }
+
+    @Override
+    public boolean isFoggyAt(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean renderClouds(@NotNull ClientLevel level, int ticks, float partialTick, double camX, double camY, double camZ, @NotNull Matrix4f modelViewMatrix, @NotNull Matrix4f projectionMatrix) {
+        return false;
+    }
+
+    @Override
+    public boolean renderSky(@NotNull ClientLevel level, int ticks, float partialTick, @NotNull Matrix4f modelViewMatrix, @NotNull Camera camera, @NotNull Matrix4f projectionMatrix, @NotNull Runnable setupFog) {
+        return false;
+    }
+
+    @Override
+    public boolean renderSnowAndRain(@NotNull ClientLevel level, int ticks, float partialTick, double camX, double camY, double camZ) {
+        return false;
+    }
+
+    @Override
+    public boolean tickRain(@NotNull ClientLevel level, int ticks, @NotNull Camera camera) {
+        return false;
+    }
+}

src/main/java/com/wanderersoftherift/wotr/world/level/RiftDimensionType.java
@@ -1,0 +1,37 @@
+package com.wanderersoftherift.wotr.world.level;
+
+import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.dimension.DimensionType;
+
+import java.util.OptionalLong;
+
+public class RiftDimensionType {
+    public static final DimensionType RIFT_DIMENSION;
+    public static final RiftDimensionSpecialEffects RIFT_DIMENSION_RENDERER;
+    public static final ResourceLocation RIFT_DIMENSION_RENDERER_KEY = WanderersOfTheRift.id("rift_dimension_renderer");
+
+    static {
+        RIFT_DIMENSION = new DimensionType(
+                OptionalLong.of(0L),
+                false,
+                false,
+                false,
+                true,
+                1,
+                false,
+                false,
+                -64, // Minimum Y Level
+                64 + 320, // Height + Min Y = Max Y
+                64 + 320, // Logical Height
+                BlockTags.INFINIBURN_OVERWORLD,
+                RiftDimensionType.RIFT_DIMENSION_RENDERER_KEY,
+                0f,
+                new DimensionType.MonsterSettings(false, false, UniformInt.of(0, 0), 0) // unsure what values we want here
+        );
+
+        RIFT_DIMENSION_RENDERER = new RiftDimensionSpecialEffects();
+    }
+}

src/main/java/com/wanderersoftherift/wotr/world/level/TemporaryLevel.java
@@ -1,0 +1,22 @@
+package com.wanderersoftherift.wotr.world.level;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+public class TemporaryLevel extends ServerLevel {
+    private final ResourceLocation id;
+
+    public TemporaryLevel(ResourceLocation id) {
+        super(
+                ServerLifecycleHooks.getCurrentServer(),
+                // ???
+        );
+
+        this.id = id;
+    }
+
+    public ResourceLocation getId() {
+        return id;
+    }
+}

src/main/java/com/wanderersoftherift/wotr/world/level/TemporaryLevelManager.java
@@ -1,0 +1,45 @@
+package com.wanderersoftherift.wotr.world.level;
+
+import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import com.wanderersoftherift.wotr.network.S2CLevelListUpdatePacket;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class TemporaryLevelManager {
+    private static final List<TemporaryLevel> levels = new ArrayList<>();
+
+    public static TemporaryLevel createRiftLevel() {
+        ResourceLocation id = WanderersOfTheRift.id("rift_" + UUID.randomUUID());
+
+        TemporaryLevel level = new TemporaryLevel(id);
+        registerLevel(level);
+        return level;
+    }
+
+    private static void registerLevel(TemporaryLevel level) {
+        Optional<Registry<Level>> dimensionRegistry = level.registryAccess().lookup(Registries.DIMENSION);
+        if (dimensionRegistry.isEmpty()) {
+            return;
+        }
+        ResourceLocation id = level.getId();
+        Registry<Level> registry = dimensionRegistry.get();
+        Registry.register(registry, id, level);
+        PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, false));
+        TemporaryLevelManager.levels.add(level);
+    }
+
+    public static void unregisterLevel(TemporaryLevel level) {
+        // idk how to deregister stuff
+        ResourceLocation id = level.getId();
+        PacketDistributor.sendToAllPlayers(new S2CLevelListUpdatePacket(id, true));
+        TemporaryLevelManager.levels.remove(level);
+    }
+}