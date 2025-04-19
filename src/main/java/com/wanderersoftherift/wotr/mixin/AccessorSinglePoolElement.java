12b06554ea15902eff727d4a62fdb14e66051a9b
ImplementsLegend <thsmpsns50@gmail.com>
2025-04-19 11:10:48 +0200

    feat: structure related accessors

---
+++ src/main/java/com/wanderersoftherift/wotr/mixin/AccessorSinglePoolElement.java
@@ -0,0 +1,18 @@
+package com.wanderersoftherift.wotr.mixin;
+
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.levelgen.structure.pools.SinglePoolElement;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(SinglePoolElement.class)
+public interface AccessorSinglePoolElement {
+    @Invoker
+    StructureTemplate callGetTemplate(StructureTemplateManager manager);
+
+    @Accessor Holder<StructureProcessorList> getProcessors();
+}
