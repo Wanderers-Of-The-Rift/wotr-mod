16a0c240f7cb5f53b63d102292cf4e827822b13a
ImplementsLegend <thsmpsns50@gmail.com>
2025-04-27 21:09:15 +0200

    feat: custom templates

---
+++ src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/template/BasicRiftTemplate.java
@@ -0,0 +1,123 @@
+package com.wanderersoftherift.wotr.world.level.levelgen.template;
+
+import com.wanderersoftherift.wotr.util.TripleMirror;
+import com.wanderersoftherift.wotr.world.level.levelgen.RiftProcessedChunk;
+import com.wanderersoftherift.wotr.world.level.levelgen.RiftProcessedRoom;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.*;
+
+import java.util.*;
+
+//todo load and spawn entities
+public class BasicRiftTemplate implements RiftGeneratable {
+    public static final int CHUNK_WIDTH = 16;
+
+    private final BlockState[][] data;
+    private final Vec3i size;
+    private final StructurePlaceSettings settings;
+    private final HashMap<Vec3i, CompoundTag> tileEntities;
+    private final List<StructureTemplate.JigsawBlockInfo> jigsaws;
+
+
+    public BasicRiftTemplate(BlockState[][] data, Vec3i size, StructurePlaceSettings settings, HashMap<Vec3i, CompoundTag> tileEntities, List<StructureTemplate.JigsawBlockInfo> jigsaws) {
+        this.data = data;
+        this.size = size;
+        this.settings = settings;
+        this.tileEntities = tileEntities;
+        this.jigsaws = jigsaws;
+    }
+
+
+    @Override
+    public Vec3i size() {
+        return size;
+    }
+
+    @Override
+    public Collection<StructureTemplate.JigsawBlockInfo> jigsaws() {
+        return jigsaws;
+    }
+
+    @Override
+    public void processAndPlace(RiftProcessedRoom destination, ServerLevel world, Vec3i placementShift, TripleMirror mirror){
+        var threads = new ArrayList<Thread>();
+        var offset = new BlockPos(destination.space.origin().multiply(16)).offset(placementShift);
+        var mutablePosition = new BlockPos.MutableBlockPos();
+        var xLastChunkPosition = 0;
+        var yLastChunkPosition = 0;
+        var zLastChunkPosition = 0;
+        RiftProcessedChunk roomChunk=null;
+        for (int i = 0; i < data.length; i++) {
+            var _i = i;
+            //threads.add(Thread.startVirtualThread(()->{
+                var templateChunk = data[_i];
+
+                for (int j = 0; j < templateChunk.length; j++) {
+                    var blockState = mirror.applyToBlockState(templateChunk[j]);
+                    if(blockState!=null) {
+                        var blockPos = new Vec3i(_i*CHUNK_WIDTH+j % CHUNK_WIDTH, (j / CHUNK_WIDTH) / size.getZ(), (j / CHUNK_WIDTH) % size.getZ());
+
+                        mutablePosition.set(offset).move(mirror.applyToPosition(blockPos, size.getX()-1, size.getZ()-1));
+
+                        var xChunkPosition = mutablePosition.getX() >> 4;
+                        var yChunkPosition = mutablePosition.getY() >> 4;
+                        var zChunkPosition = mutablePosition.getZ() >> 4;
+
+                        if (xLastChunkPosition!=xChunkPosition || yLastChunkPosition!=yChunkPosition || zLastChunkPosition!=zChunkPosition || roomChunk==null){
+                            xLastChunkPosition = xChunkPosition;
+                            yLastChunkPosition = yChunkPosition;
+                            zLastChunkPosition = zChunkPosition;
+                            roomChunk = destination.getOrCreateChunk(new Vec3i(xChunkPosition,yChunkPosition,zChunkPosition));
+                        }
+                        var xWithinChunk = mutablePosition.getX() & 0xf;
+                        var yWithinChunk = mutablePosition.getY() & 0xf;
+                        var zWithinChunk = mutablePosition.getZ() & 0xf;
+                        if(roomChunk.blocks[(xWithinChunk) | ((zWithinChunk) <<4) | ((yWithinChunk) <<8)]!=null)continue;
+                        var nbt = tileEntities.get(blockPos);
+                        nbt=nbt==null?new CompoundTag():nbt.copy();
+                        var info = new StructureTemplate.StructureBlockInfo(mutablePosition, blockState, nbt);
+                        if (settings!=null) {
+                            var relative = StructureTemplate.calculateRelativePosition(settings, mutablePosition);
+                            var original = new StructureTemplate.StructureBlockInfo(mutablePosition, blockState, nbt);
+                            info=JigsawReplacementProcessor.INSTANCE.process(world,offset,offset,original,info,settings,null);
+                            for (var processor : settings.getProcessors()) {
+
+                                info = processor.process(world, offset/*pieceOrigin*/, offset/*structureOrigin*/, original, info, settings, null);
+                            }
+                            if (info == null) continue;
+                        }
+                        var finalPos = info.pos();
+                        xChunkPosition = finalPos.getX() >> 4;
+                        yChunkPosition = finalPos.getY() >> 4;
+                        zChunkPosition = finalPos.getZ() >> 4;
+
+                        if (xLastChunkPosition!=xChunkPosition || yLastChunkPosition!=yChunkPosition || zLastChunkPosition!=zChunkPosition || roomChunk==null){
+                            xLastChunkPosition = xChunkPosition;
+                            yLastChunkPosition = yChunkPosition;
+                            zLastChunkPosition = zChunkPosition;
+                            roomChunk = destination.getOrCreateChunk(new Vec3i(xChunkPosition,yChunkPosition,zChunkPosition));
+                        }
+                        xWithinChunk = finalPos.getX() & 0xf;
+                        yWithinChunk = finalPos.getY() & 0xf;
+                        zWithinChunk = finalPos.getZ() & 0xf;
+                        roomChunk.blocks[(xWithinChunk) | ((zWithinChunk) <<4) | ((yWithinChunk) <<8)]=info.state();
+                        nbt=info.nbt();
+                        if(nbt!=null && !nbt.isEmpty())roomChunk.blockNBT[(xWithinChunk) | ((zWithinChunk) <<4) | ((yWithinChunk) <<8)] = nbt;
+                    }
+                }
+           // }));
+        }
+        for (var thread:threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/template/RiftGeneratable.java
@@ -0,0 +1,98 @@
+package com.wanderersoftherift.wotr.world.level.levelgen.template;
+
+import com.wanderersoftherift.wotr.util.TripleMirror;
+import com.wanderersoftherift.wotr.world.level.levelgen.RiftProcessedRoom;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.JigsawBlock;
+import net.minecraft.world.level.block.entity.JigsawBlockEntity;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+
+import java.util.Collection;
+
+public interface RiftGeneratable {
+
+    void processAndPlace(RiftProcessedRoom destination, ServerLevel world, Vec3i placementShift, TripleMirror mirror);
+
+    Collection<StructureTemplate.JigsawBlockInfo> jigsaws();
+
+    Vec3i size();
+
+
+    public static void generate(RiftGeneratable generatable, RiftProcessedRoom destination, ServerLevel world, Vec3i placementShift, TripleMirror mirror, MinecraftServer server, RandomSource random){
+
+        for (var jigsaw:generatable.jigsaws()){
+            var pool = jigsaw.pool();
+            if (isPoolBlacklisted(pool)) {
+                continue;
+            }
+            var next = RiftTemplates.random(server,pool,null,random);
+            if(next==null) {
+                continue;
+            }
+
+            var simplifiedDirection1 = simplifiedDirection(jigsaw,mirror);
+            var simplifiedDirection1Opposite = simplifiedDirection1.getOpposite();
+
+            var jigsaw2List = next.jigsaws().stream().filter(
+                    (otherJigsaw)->{
+                        var otherSimplifiedDirection = simplifiedDirection(otherJigsaw, TripleMirror.NONE);
+                        return otherJigsaw.name().equals(jigsaw.target()) && (simplifiedDirection1Opposite==otherSimplifiedDirection || (simplifiedDirection1.getStepY()==0 && otherSimplifiedDirection.getStepY()==0 /*checks if both directions are horizontal*/));
+                    }).toList();
+
+            if(jigsaw2List.isEmpty()) continue;//todo possibly multiple attempts
+            var jigsaw2 = jigsaw2List.get(random.nextInt(jigsaw2List.size()));
+            var simplifiedDirection2 = simplifiedDirection(jigsaw2,TripleMirror.NONE);
+            var nextMirrorInt = random.nextInt(8);
+            if(simplifiedDirection1.getAxis()== Direction.Axis.Y){
+                if (simplifiedDirection2!=simplifiedDirection1Opposite){
+                    continue;
+                }
+                if(jigsaw.jointType()== JigsawBlockEntity.JointType.ALIGNED && jigsaw2.jointType()== JigsawBlockEntity.JointType.ALIGNED){
+                    nextMirrorInt = mirrorCorrection(auxiliaryDirection(jigsaw,mirror), auxiliaryDirection(jigsaw2,TripleMirror.NONE),nextMirrorInt,false);
+                }
+            }else {
+                nextMirrorInt = mirrorCorrection(simplifiedDirection1,simplifiedDirection2,nextMirrorInt,true);
+            }
+            var nextMirror = new TripleMirror(nextMirrorInt);
+            var newPlacementShift = placementShift.offset(mirror.applyToPosition(jigsaw.info().pos(), generatable.size().getX()-1, generatable.size().getZ()-1)).offset(nextMirror.applyToPosition(jigsaw2.info().pos(),next.size().getX()-1,next.size().getZ()-1).multiply(-1));
+
+            generate(next,destination, world, newPlacementShift.relative(simplifiedDirection1), nextMirror, server, random);
+        }
+        generatable.processAndPlace(destination, world, placementShift, mirror);
+    }
+
+
+    private static int setOrClearBit(int value, int bit, boolean set){
+        value &=~bit;
+        if(set){
+            value|=bit;
+        }
+        return value;
+    }
+
+    private static int mirrorCorrection(Direction d1, Direction d2,int oldMirrorInt, boolean targetOpposite){
+        return setOrClearBit(
+                setOrClearBit(oldMirrorInt, 0b100, d1.getAxis()!=d2.getAxis()),
+                d2.getAxis()== Direction.Axis.Z?0b010:0b001,
+                ((d1.getStepX()+d1.getStepZ()) > 0 == (d2.getStepX()+d2.getStepZ()) > 0) == targetOpposite
+        );
+    }
+
+    private static Direction simplifiedDirection(StructureTemplate.JigsawBlockInfo jigsaw,TripleMirror mirror) {
+        return mirror.applyToBlockState(jigsaw.info().state()).getValue(JigsawBlock.ORIENTATION).front();
+    }
+
+    private static Direction auxiliaryDirection(StructureTemplate.JigsawBlockInfo jigsaw, TripleMirror mirror) {
+        return mirror.applyToBlockState(jigsaw.info().state()).getValue(JigsawBlock.ORIENTATION).top();
+    }
+
+    private static boolean isPoolBlacklisted(ResourceLocation pool) {
+        return pool.getPath().contains("rift/ring_") && pool.getNamespace().equals("wotr");
+    }
+
+}
