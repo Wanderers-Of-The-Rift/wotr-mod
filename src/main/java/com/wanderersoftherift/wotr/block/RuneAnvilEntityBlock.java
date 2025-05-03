9b1bd224bf672f0f9049f1dd456e5ffe3ca9ab43
mel <mcmelon@nodiumhosting.com>
2025-05-03 22:11:17 +0200

    fix the rune anvil not dropping it's contents when broken

--- src/main/java/com/wanderersoftherift/wotr/block/RuneAnvilEntityBlock.java
+++ src/main/java/com/wanderersoftherift/wotr/block/RuneAnvilEntityBlock.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.MapCodec;
 import com.wanderersoftherift.wotr.block.blockentity.RuneAnvilBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
@@ -57,5 +58,16 @@ public class RuneAnvilEntityBlock extends BaseEntityBlock {
     @Override
     public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos blockPos, @NotNull BlockState blockState) {
         return new RuneAnvilBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    protected void onRemove(
+            @NotNull BlockState state,
+            @NotNull Level level,
+            @NotNull BlockPos pos,
+            @NotNull BlockState newState,
+            boolean movedByPiston) {
+        Containers.dropContentsOnDestroy(state, newState, level, pos);
+        super.onRemove(state, level, pos, newState, movedByPiston);
     }
 }
