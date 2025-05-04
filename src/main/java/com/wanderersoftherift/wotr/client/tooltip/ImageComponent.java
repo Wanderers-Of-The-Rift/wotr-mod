c7a4d0c4a57db8f00443de79f522d542e19bda7d
mel <mcmelon@nodiumhosting.com>
2025-05-04 03:41:25 +0200

    adsjkhghjfdgh

--- src/main/java/com/wanderersoftherift/wotr/client/tooltip/ImageComponent.java
+++ src/main/java/com/wanderersoftherift/wotr/client/tooltip/ImageComponent.java
@@ -5,8 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 
-public record ImageComponent(ItemStack stack, Component base,
-                             ResourceLocation asset) implements TooltipComponent { // TODO:
+public record ImageComponent(ItemStack stack, Component base, ResourceLocation asset) implements TooltipComponent { // TODO:
     // See
     // if
     // the
