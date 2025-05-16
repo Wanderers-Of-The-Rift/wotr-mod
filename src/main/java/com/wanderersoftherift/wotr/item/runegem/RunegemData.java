98d85492d8cdd80d9d74dc692fd49babfbd457dd
Patrigan <patrick.provinciael@gmail.com>
2025-05-16 11:01:32 +0200

    fix: SPOT LEEEEEESS

--- src/main/java/com/wanderersoftherift/wotr/item/runegem/RunegemData.java
+++ src/main/java/com/wanderersoftherift/wotr/item/runegem/RunegemData.java
@@ -10,7 +10,6 @@ import net.minecraft.core.RegistryCodecs;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
