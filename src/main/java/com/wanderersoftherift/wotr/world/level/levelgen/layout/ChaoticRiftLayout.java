3e8df37d13502f5f526d6d37f12a307871a83521
ImplementsLegend <thsmpsns50@gmail.com>
2025-05-22 16:59:54 +0200

    adjusted default shape of ChaoticRiftLayout.java

--- src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/layout/ChaoticRiftLayout.java
+++ src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/layout/ChaoticRiftLayout.java
@@ -166,7 +166,7 @@ public class ChaoticRiftLayout implements RiftLayout {
         }
 
         private double chaosiveness(double x, double z) {
-            return 1.5 * Math.cosh(0.055 * Math.sqrt(x * x + z * z));
+            return 1.5 * Math.cosh(0.11 * Math.sqrt(x * x + z * z));
         } // todo move to interface
 
         // 2 = chaotic, 1 = unstable, 0 = stable
