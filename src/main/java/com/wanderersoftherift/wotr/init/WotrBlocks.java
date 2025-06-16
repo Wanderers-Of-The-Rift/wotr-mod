3ff2a23be94f380f06c508a52a9c79df5357ec52
Jutta <jutta.k.lehtonen@gmail.com>
2025-06-16 19:42:56 +0300

    Relocating stuff.

--- src/main/java/com/wanderersoftherift/wotr/init/WotrBlocks.java
+++ src/main/java/com/wanderersoftherift/wotr/init/WotrBlocks.java
@@ -132,120 +132,6 @@ public class WotrBlocks {
                     .sound(SoundType.WOOD)
                     .noOcclusion()));
 
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_1 = registerBuildingBlock("processor_block_1",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_1"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_2 = registerBuildingBlock("processor_block_2",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_2"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_3 = registerBuildingBlock("processor_block_3",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_3"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_4 = registerBuildingBlock("processor_block_4",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_4"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_5 = registerBuildingBlock("processor_block_5",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_5"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_6 = registerBuildingBlock("processor_block_6",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_6"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_7 = registerBuildingBlock("processor_block_7",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_7"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_8 = registerBuildingBlock("processor_block_8",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_8"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_9 = registerBuildingBlock("processor_block_9",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_9"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_10 = registerBuildingBlock("processor_block_10",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_10"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_11 = registerBuildingBlock("processor_block_11",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_11"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_12 = registerBuildingBlock("processor_block_12",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_12"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_13 = registerBuildingBlock("processor_block_13",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_13"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_14 = registerBuildingBlock("processor_block_14",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_14"))));
-    public static final BlockFamilyHelper PROCESSOR_BLOCK_15 = registerBuildingBlock("processor_block_15",
-            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_15"))));
-
-    private static BlockFamilyHelper registerBuildingBlock(String id, Supplier<Block> sup) {
-        DeferredBlock<Block> block = registerDevBlock(id, sup);
-        BlockFamilyHelper buildingBlockHelper = new BlockFamilyHelper.Builder().withBlockId(id)
-                .withBlock(block)
-                .withSlab(registerDevBlock(id + SLAB_SUFFIX, () -> new SlabBlock(
-                        BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + SLAB_SUFFIX)))))
-                .withStairs(registerDevBlock(id + STAIRS_SUFFIX,
-                        () -> new StairBlock(block.get().defaultBlockState(),
-                                BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + STAIRS_SUFFIX)))))
-                .withButton(registerDevBlock(id + BUTTON_SUFFIX,
-                        () -> new ButtonBlock(BlockSetType.STONE, 30,
-                                BlockBehaviour.Properties.ofFullCopy(block.get())
-                                        .noCollission()
-                                        .strength(0.5F)
-                                        .setId(blockId(id + BUTTON_SUFFIX)))))
-                .withPressurePlate(registerDevBlock(id + PLATE_SUFFIX,
-                        () -> new PressurePlateBlock(BlockSetType.STONE,
-                                BlockBehaviour.Properties.ofFullCopy(block.get())
-                                        .noCollission()
-                                        .strength(0.5F)
-                                        .setId(blockId(id + PLATE_SUFFIX)))))
-                .withWall(registerDevBlock(id + WALL_SUFFIX, () -> new WallBlock(
-                        BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + WALL_SUFFIX)))))
-                .withFence(registerDevBlock(id + FENCE_SUFFIX, () -> new FenceBlock(
-                        BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + FENCE_SUFFIX)))))
-                .withFenceGate(registerDevBlock(id + FENCE_GATE_SUFFIX,
-                        () -> new FenceGateBlock(OAK,
-                                BlockBehaviour.Properties.ofFullCopy(block.get())
-                                        .setId(blockId(id + FENCE_GATE_SUFFIX)))))
-                .withTrapdoor(registerDevBlock(id + TRAPDOOR_SUFFIX,
-                        () -> new TrapDoorBlock(BlockSetType.STONE,
-                                BlockBehaviour.Properties.ofFullCopy(block.get())
-                                        .setId(blockId(id + TRAPDOOR_SUFFIX)))))
-                .withPane(
-                        registerDevBlock(id + GLASS_BLOCK_SUFFIX,
-                                () -> new StainedGlassBlock(DyeColor.WHITE,
-                                        BlockBehaviour.Properties.ofFullCopy(block.get())
-                                                .noOcclusion()
-                                                .lightLevel((state) -> 0)
-                                                .sound(SoundType.GLASS)
-                                                .setId(blockId(id + GLASS_BLOCK_SUFFIX)))),
-                        registerDevBlock(id + PANE_SUFFIX,
-                                () -> new StainedGlassPaneBlock(DyeColor.WHITE,
-                                        BlockBehaviour.Properties.ofFullCopy(block.get())
-                                                .noOcclusion()
-                                                .lightLevel((state) -> 0)
-                                                .sound(SoundType.GLASS)
-                                                .setId(blockId(id + PANE_SUFFIX)))))
-                .withDirectionalPillar(registerDevBlock(id + DIRECTIONAL_PILLAR_SUFFIX,
-                        () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(block.get())
-                                .setId(blockId(id + DIRECTIONAL_PILLAR_SUFFIX))) {
-                        }))
-                .createBuildBlockHelper();
-        BLOCK_FAMILY_HELPERS.add(buildingBlockHelper);
-        return buildingBlockHelper;
-    }
-
-    private static <T extends Block> DeferredBlock<T> registerChestBlock(
-            String riftChest,
-            Supplier<T> supplier,
-            RiftChestType riftChestType) {
-        DeferredBlock<T> register = registerBlock(riftChest, supplier);
-        CHEST_TYPES.put(riftChestType, (DeferredBlock<Block>) register);
-        return register;
-    }
-
-    private static <T extends Block> DeferredBlock<T> registerBlock(String key, Supplier<T> sup) {
-        DeferredBlock<T> register = BLOCKS.register(key, sup);
-        WotrItems.registerSimpleBlockItem(key, register);
-        return register;
-    }
-
-    private static <T extends Block> DeferredBlock<T> registerDevBlock(String key, Supplier<T> sup) {
-        DeferredBlock<T> register = BLOCKS.register(key, sup);
-        WotrItems.registerSimpleDevBlockItem(key, register);
-        return register;
-    }
-
-    private static ResourceKey<Block> blockId(String name) {
-        return ResourceKey.create(Registries.BLOCK,
-                ResourceLocation.fromNamespaceAndPath(WanderersOfTheRift.MODID, name));
-    }
-
     public static final DeferredBlock<Block> NOGRAVGRAVEL = registerBlock("nograv_gravel",
             () -> new Block(BlockBehaviour.Properties.of()
                     .setId(blockId("nograv_gravel"))
@@ -360,4 +246,117 @@ public class WotrBlocks {
                     .strength(0.6F)
                     .sound(SoundType.SAND)));
 
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_1 = registerBuildingBlock("processor_block_1",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_1"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_2 = registerBuildingBlock("processor_block_2",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_2"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_3 = registerBuildingBlock("processor_block_3",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_3"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_4 = registerBuildingBlock("processor_block_4",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_4"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_5 = registerBuildingBlock("processor_block_5",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_5"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_6 = registerBuildingBlock("processor_block_6",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_6"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_7 = registerBuildingBlock("processor_block_7",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_7"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_8 = registerBuildingBlock("processor_block_8",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_8"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_9 = registerBuildingBlock("processor_block_9",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_9"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_10 = registerBuildingBlock("processor_block_10",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_10"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_11 = registerBuildingBlock("processor_block_11",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_11"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_12 = registerBuildingBlock("processor_block_12",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_12"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_13 = registerBuildingBlock("processor_block_13",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_13"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_14 = registerBuildingBlock("processor_block_14",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_14"))));
+    public static final BlockFamilyHelper PROCESSOR_BLOCK_15 = registerBuildingBlock("processor_block_15",
+            () -> new Block(BlockBehaviour.Properties.of().setId(blockId("processor_block_15"))));
+
+    private static BlockFamilyHelper registerBuildingBlock(String id, Supplier<Block> sup) {
+        DeferredBlock<Block> block = registerDevBlock(id, sup);
+        BlockFamilyHelper buildingBlockHelper = new BlockFamilyHelper.Builder().withBlockId(id)
+                .withBlock(block)
+                .withSlab(registerDevBlock(id + SLAB_SUFFIX, () -> new SlabBlock(
+                        BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + SLAB_SUFFIX)))))
+                .withStairs(registerDevBlock(id + STAIRS_SUFFIX,
+                        () -> new StairBlock(block.get().defaultBlockState(),
+                                BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + STAIRS_SUFFIX)))))
+                .withButton(registerDevBlock(id + BUTTON_SUFFIX,
+                        () -> new ButtonBlock(BlockSetType.STONE, 30,
+                                BlockBehaviour.Properties.ofFullCopy(block.get())
+                                        .noCollission()
+                                        .strength(0.5F)
+                                        .setId(blockId(id + BUTTON_SUFFIX)))))
+                .withPressurePlate(registerDevBlock(id + PLATE_SUFFIX,
+                        () -> new PressurePlateBlock(BlockSetType.STONE,
+                                BlockBehaviour.Properties.ofFullCopy(block.get())
+                                        .noCollission()
+                                        .strength(0.5F)
+                                        .setId(blockId(id + PLATE_SUFFIX)))))
+                .withWall(registerDevBlock(id + WALL_SUFFIX, () -> new WallBlock(
+                        BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + WALL_SUFFIX)))))
+                .withFence(registerDevBlock(id + FENCE_SUFFIX, () -> new FenceBlock(
+                        BlockBehaviour.Properties.ofFullCopy(block.get()).setId(blockId(id + FENCE_SUFFIX)))))
+                .withFenceGate(registerDevBlock(id + FENCE_GATE_SUFFIX,
+                        () -> new FenceGateBlock(OAK,
+                                BlockBehaviour.Properties.ofFullCopy(block.get())
+                                        .setId(blockId(id + FENCE_GATE_SUFFIX)))))
+                .withTrapdoor(registerDevBlock(id + TRAPDOOR_SUFFIX,
+                        () -> new TrapDoorBlock(BlockSetType.STONE,
+                                BlockBehaviour.Properties.ofFullCopy(block.get())
+                                        .setId(blockId(id + TRAPDOOR_SUFFIX)))))
+                .withPane(
+                        registerDevBlock(id + GLASS_BLOCK_SUFFIX,
+                                () -> new StainedGlassBlock(DyeColor.WHITE,
+                                        BlockBehaviour.Properties.ofFullCopy(block.get())
+                                                .noOcclusion()
+                                                .lightLevel((state) -> 0)
+                                                .sound(SoundType.GLASS)
+                                                .setId(blockId(id + GLASS_BLOCK_SUFFIX)))),
+                        registerDevBlock(id + PANE_SUFFIX,
+                                () -> new StainedGlassPaneBlock(DyeColor.WHITE,
+                                        BlockBehaviour.Properties.ofFullCopy(block.get())
+                                                .noOcclusion()
+                                                .lightLevel((state) -> 0)
+                                                .sound(SoundType.GLASS)
+                                                .setId(blockId(id + PANE_SUFFIX)))))
+                .withDirectionalPillar(registerDevBlock(id + DIRECTIONAL_PILLAR_SUFFIX,
+                        () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(block.get())
+                                .setId(blockId(id + DIRECTIONAL_PILLAR_SUFFIX))) {
+                        }))
+                .createBuildBlockHelper();
+        BLOCK_FAMILY_HELPERS.add(buildingBlockHelper);
+        return buildingBlockHelper;
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerChestBlock(
+            String riftChest,
+            Supplier<T> supplier,
+            RiftChestType riftChestType) {
+        DeferredBlock<T> register = registerBlock(riftChest, supplier);
+        CHEST_TYPES.put(riftChestType, (DeferredBlock<Block>) register);
+        return register;
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String key, Supplier<T> sup) {
+        DeferredBlock<T> register = BLOCKS.register(key, sup);
+        WotrItems.registerSimpleBlockItem(key, register);
+        return register;
+    }
+
+    private static <T extends Block> DeferredBlock<T> registerDevBlock(String key, Supplier<T> sup) {
+        DeferredBlock<T> register = BLOCKS.register(key, sup);
+        WotrItems.registerSimpleDevBlockItem(key, register);
+        return register;
+    }
+
+    private static ResourceKey<Block> blockId(String name) {
+        return ResourceKey.create(Registries.BLOCK,
+                ResourceLocation.fromNamespaceAndPath(WanderersOfTheRift.MODID, name));
+    }
 }
