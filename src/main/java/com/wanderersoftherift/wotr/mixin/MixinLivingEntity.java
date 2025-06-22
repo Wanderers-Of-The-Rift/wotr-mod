f7645f30f701faa6bbd3cbf588043247bdad17af
attack8 <attackeight@gmail.com>
2025-06-21 23:17:27 -0400

    Prevent Mob Drops in Rifts Fixes #248

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
@@ -1,14 +1,17 @@
 package com.wanderersoftherift.wotr.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import com.wanderersoftherift.wotr.core.rift.RiftLevelManager;
 import com.wanderersoftherift.wotr.modifier.ModifierHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,7 +20,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Map;
 
 @Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+public abstract class MixinLivingEntity {
+
+    @Shadow protected abstract boolean shouldDropLoot();
 
     @Inject(method = "onChangedBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;runLocationChangedEffects(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;)V"))
     private void onChangedBlockRunLocationChangedEffects(ServerLevel level, BlockPos pos, CallbackInfo ci) {
@@ -42,6 +47,11 @@ public class MixinLivingEntity {
             CallbackInfo ci) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
         ModifierHelper.disableModifier(stack, livingEntity, slot);
+    }
+
+    @Inject(method = "dropAllDeathLoot", at = @At(value = "HEAD"), cancellable = true)
+    private void stopDeathLootInRift(ServerLevel sLevel, DamageSource damageSource, CallbackInfo ci) {
+        if (RiftLevelManager.isRift(sLevel)) ci.cancel();
     }
 
 }
