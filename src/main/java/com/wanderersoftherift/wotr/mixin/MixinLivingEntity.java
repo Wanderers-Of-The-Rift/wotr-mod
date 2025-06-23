622a2f80ea5f3b5e30f960013773c49e7f53cea1
attack8 <attackeight@gmail.com>
2025-06-23 11:22:52 -0400

    Remove Unused Import

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
@@ -11,7 +11,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
