87190b829f6189e093038ec1baed1d3bdd1ba175
ImplementsLegend <thsmpsns50@gmail.com>
2025-07-01 12:54:01 +0200

    spotless once more

--- src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/processor/util/ProcessorUtil.java
+++ src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/processor/util/ProcessorUtil.java
@@ -13,7 +13,6 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerChunkCache;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
