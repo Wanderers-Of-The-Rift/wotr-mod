7c12b5aab652d9749d4a3a65b7b3fbf589753388
ImplementsLegend <thsmpsns50@gmail.com>
2025-07-07 11:55:18 +0200

    refactor: renamed IterateEquipmentSlotsEvent to CollectEquipmentSlotsEvent, made WotrEquipmentSlotFromMC.SLOTS immutable

--- src/main/java/com/wanderersoftherift/wotr/modifier/WotrEquipmentSlotFromMC.java
+++ src/main/java/com/wanderersoftherift/wotr/modifier/WotrEquipmentSlotFromMC.java
@@ -1,27 +1,30 @@
 package com.wanderersoftherift.wotr.modifier;
 
+import com.google.common.collect.ImmutableList;
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.attachment.IAttachmentHolder;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.Map;
+import java.util.List;
 
 public record WotrEquipmentSlotFromMC(EquipmentSlot minecraftSlot, TagKey<Item> tag) implements WotrEquipmentSlot {
 
-    public static final ArrayList<WotrEquipmentSlotFromMC> SLOTS;
+    // has same order as net.minecraft.world.entity.EquipmentSlot enum
+    public static final List<WotrEquipmentSlotFromMC> SLOTS;
 
-    public static final @NotNull Map<EquipmentSlot, WotrEquipmentSlotFromMC> SLOT_MAP;
+    public static WotrEquipmentSlot fromVanillaSlot(EquipmentSlot vanillaSlot) {
+        return SLOTS.get(vanillaSlot.ordinal());
+    }
 
     @Override
     public boolean canAccept(ItemStack stack) {
@@ -47,32 +50,40 @@ public record WotrEquipmentSlotFromMC(EquipmentSlot minecraftSlot, TagKey<Item> 
     }
 
     static {
-        var slots = new ArrayList<WotrEquipmentSlotFromMC>();
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.MAINHAND,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_main_hand_slot"))));
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.OFFHAND,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_off_hand_slot"))));
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.FEET,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_boots_slot"))));
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.LEGS,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_leggings_slot"))));
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.BODY,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_chestplate_slot"))));
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.CHEST,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_chestplate_slot"))));
-        slots.add(new WotrEquipmentSlotFromMC(EquipmentSlot.HEAD,
-                TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_helmet_slot"))));
-        SLOTS = slots;
-        SLOT_MAP = new EnumMap<>(
-                Map.ofEntries(slots.stream().map(it -> Map.entry(it.minecraftSlot, it)).toArray(Map.Entry[]::new)));
+        SLOTS = ImmutableList.of(
+                new WotrEquipmentSlotFromMC(EquipmentSlot.MAINHAND,
+                        TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_main_hand_slot"))),
+                new WotrEquipmentSlotFromMC(EquipmentSlot.OFFHAND,
+                        TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_off_hand_slot"))),
+                new WotrEquipmentSlotFromMC(EquipmentSlot.FEET,
+                        TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_boots_slot"))),
+                new WotrEquipmentSlotFromMC(EquipmentSlot.LEGS,
+                        TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_leggings_slot"))),
+                new WotrEquipmentSlotFromMC(EquipmentSlot.CHEST,
+                        TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_chestplate_slot"))),
+                new WotrEquipmentSlotFromMC(EquipmentSlot.HEAD,
+                        TagKey.create(Registries.ITEM, WanderersOfTheRift.id("socketable_helmet_slot"))),
+                // this is apparently slot for horse armor
+                new WotrEquipmentSlotFromMC(EquipmentSlot.BODY,
+                        TagKey.create(Registries.ITEM, ResourceLocation.parse("minecraft:empty"))));
     }
 
     @EventBusSubscriber(modid = WanderersOfTheRift.MODID)
     public static class Register {
 
         @SubscribeEvent
-        private static void provideMinecraftSlots(ModifierHelper.IterateEquipmentSlotsEvent event) {
-            event.getSlots().addAll(SLOTS);
+        private static void collectMinecraftSlots(ModifierHelper.CollectEquipmentSlotsEvent event) {
+            if (event.getEntity() instanceof LivingEntity) {
+                if (event.getEntity() instanceof Animal) {
+                    SLOTS.stream()
+                            .filter(slot -> slot.minecraftSlot.getType() == EquipmentSlot.Type.ANIMAL_ARMOR)
+                            .forEach(event.getSlots()::add);
+                } else {
+                    SLOTS.stream()
+                            .filter(slot -> slot.minecraftSlot.getType() != EquipmentSlot.Type.ANIMAL_ARMOR)
+                            .forEach(event.getSlots()::add);
+                }
+            }
         }
     }
 }
