a10727d385c748e49bef8741894ae8ab545677a7
Rev219 <c.mccarron770@gmail.com>
2025-07-10 14:02:09 -0400

    fix: cleanup

--- src/main/java/com/wanderersoftherift/wotr/effects/events/OnHitEvent.java
+++ src/main/java/com/wanderersoftherift/wotr/effects/events/OnHitEvent.java
@@ -5,17 +5,12 @@ import com.wanderersoftherift.wotr.effects.CriticalEffect;
 import com.wanderersoftherift.wotr.effects.LifeLeechEffect;
 import com.wanderersoftherift.wotr.effects.ThornsEffect;
 import com.wanderersoftherift.wotr.init.WotrDamageTypes;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.damagesource.DamageContainer;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
