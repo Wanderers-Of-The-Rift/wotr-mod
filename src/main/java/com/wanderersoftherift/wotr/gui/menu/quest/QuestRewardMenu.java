707c03ffac9fce9efd4b6d853ae0aa4602cc6e64
immortius <immortius@gmail.com>
2025-07-21 22:04:14 +1000

    refactor: decoupled quest completion from the quest reward screen

--- src/main/java/com/wanderersoftherift/wotr/gui/menu/quest/QuestRewardMenu.java
+++ src/main/java/com/wanderersoftherift/wotr/gui/menu/quest/QuestRewardMenu.java
@@ -1,24 +1,20 @@
 package com.wanderersoftherift.wotr.gui.menu.quest;
 
-import com.wanderersoftherift.wotr.core.guild.quest.ActiveQuests;
-import com.wanderersoftherift.wotr.core.guild.quest.QuestState;
 import com.wanderersoftherift.wotr.core.guild.quest.Reward;
 import com.wanderersoftherift.wotr.gui.menu.QuickMover;
-import com.wanderersoftherift.wotr.init.WotrAttachments;
 import com.wanderersoftherift.wotr.init.WotrMenuTypes;
 import com.wanderersoftherift.wotr.util.ItemStackHandlerUtil;
-import net.minecraft.client.Minecraft;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
-import net.minecraft.world.inventory.DataSlot;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import net.neoforged.neoforge.items.SlotItemHandler;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,29 +35,21 @@ public class QuestRewardMenu extends AbstractContainerMenu {
             .build();
 
     private final ContainerLevelAccess access;
-    private final ActiveQuests activeQuests;
-    private final DataSlot selectedQuest;
-
-    private final ItemStackHandler rewardItems;
+    private final ItemStackHandler itemRewards;
+    private final List<Reward> nonItemRewards = new ArrayList<>();
 
     public QuestRewardMenu(int containerId, Inventory playerInventory) {
-        this(containerId, playerInventory, ContainerLevelAccess.NULL,
-                Minecraft.getInstance().player.getData(WotrAttachments.ACTIVE_QUESTS), 0);
+        this(containerId, playerInventory, ContainerLevelAccess.NULL);
     }
 
-    public QuestRewardMenu(int containerId, Inventory playerInventory, ContainerLevelAccess access,
-            ActiveQuests activeQuests, int questIndex) {
+    public QuestRewardMenu(int containerId, Inventory playerInventory, ContainerLevelAccess access) {
         super(WotrMenuTypes.QUEST_REWARD_MENU.get(), containerId);
 
         this.access = access;
-        this.activeQuests = activeQuests;
-        this.rewardItems = new ItemStackHandler(NUM_REWARD_SLOTS);
-
-        this.selectedQuest = addDataSlot(DataSlot.standalone());
-        selectedQuest.set(questIndex);
+        this.itemRewards = new ItemStackHandler(NUM_REWARD_SLOTS);
 
         for (int i = 0; i < NUM_REWARD_SLOTS; i++) {
-            this.addSlot(new SlotItemHandler(rewardItems, i, 8 + 18 * i, 49));
+            this.addSlot(new SlotItemHandler(itemRewards, i, 8 + 18 * i, 49));
         }
         addStandardInventorySlots(playerInventory, 8, 80);
     }
@@ -77,35 +65,30 @@ public class QuestRewardMenu extends AbstractContainerMenu {
     }
 
     public List<Reward> getNonItemRewards() {
-        return activeQuests.getQuestState(selectedQuest.get()).getRewards().stream().filter(x -> !x.isItem()).toList();
+        return nonItemRewards;
     }
 
     @Override
     public void removed(@NotNull Player player) {
         super.removed(player);
-        QuestState questState = activeQuests.getQuestState(selectedQuest.get());
         if (player instanceof ServerPlayer serverPlayer) {
-            ItemStackHandlerUtil.placeInPlayerInventoryOrDrop(serverPlayer, rewardItems);
-            List<Reward> rewards = questState.getRewards();
-            for (Reward reward : rewards) {
+            ItemStackHandlerUtil.placeInPlayerInventoryOrDrop(serverPlayer, itemRewards);
+            for (Reward reward : nonItemRewards) {
                 reward.apply(serverPlayer);
             }
-            // Remove the quest so the player cannot take it again, if it is still available
-            player.getData(WotrAttachments.AVAILABLE_QUESTS).removeIf(x -> x.getId().equals(questState.getId()));
-            activeQuests.remove(questState.getId());
         }
     }
 
-    // TODO: Remove linkage to quest, just provide rewards and replicate to player - this allows quest to be removed
-    // earlier and prevent duplication by crashing the server/game
-    public void addRewards(ServerPlayer player) {
+    public void setRewards(Player player, List<Reward> rewards) {
+        nonItemRewards.addAll(rewards.stream().filter(x -> !x.isItem()).toList());
         access.execute((level, pos) -> {
-            QuestState quest = activeQuests.getQuestState(selectedQuest.get());
-            for (Reward reward : quest.getRewards()) {
+            if (!(player instanceof ServerPlayer serverPlayer)) {
+                return;
+            }
+            for (Reward reward : rewards) {
                 ItemStack rewardItem = reward.generateItem();
-
                 if (!rewardItem.isEmpty()) {
-                    ItemStackHandlerUtil.addOrGiveToPlayerOrDrop(rewardItem, rewardItems, player);
+                    ItemStackHandlerUtil.addOrGiveToPlayerOrDrop(rewardItem, itemRewards, serverPlayer);
                 }
             }
         });
