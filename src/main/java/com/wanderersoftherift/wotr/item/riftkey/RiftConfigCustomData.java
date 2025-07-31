ca5be9b5230c6fa88bb0ce21684eaa9f32111229
ImplementsLegend <thsmpsns50@gmail.com>
2025-07-31 12:25:22 +0200

    fix: RiftConfigCustomData.DISPATCHED_MAP_CODEC now returns mutable map

--- src/main/java/com/wanderersoftherift/wotr/item/riftkey/RiftConfigCustomData.java
+++ src/main/java/com/wanderersoftherift/wotr/item/riftkey/RiftConfigCustomData.java
@@ -9,15 +9,18 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 /*
  * By default has no implementation, intended as an option for 3rd parties to extend the riftConfig
  */
 public interface RiftConfigCustomData {
 
-    Codec<Map<Holder<MapCodec<? extends RiftConfigCustomData>>, RiftConfigCustomData>> DISPATCHED_MAP_CODEC = new DispatchedMapCodec<>(
-            WotrRegistries.RIFT_CONFIG_CUSTOM_DATA_TYPES.holderByNameCodec(), it -> it.value().codec());
+    Codec<Map<Holder<MapCodec<? extends RiftConfigCustomData>>, RiftConfigCustomData>> DISPATCHED_MAP_CODEC = new DispatchedMapCodec<Holder<MapCodec<? extends RiftConfigCustomData>>, RiftConfigCustomData>(
+            WotrRegistries.RIFT_CONFIG_CUSTOM_DATA_TYPES.holderByNameCodec(), it -> it.value().codec())
+            .xmap(HashMap::new, Function.identity());
     StreamCodec<RegistryFriendlyByteBuf, Map<Holder<MapCodec<? extends RiftConfigCustomData>>, RiftConfigCustomData>> DISPATCHED_MAP_STREAM_CODEC = ByteBufCodecs
             .fromCodecWithRegistries(DISPATCHED_MAP_CODEC);
 
