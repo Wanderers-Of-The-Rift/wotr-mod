4b88d5b641ddcd38fb019f9e012887b8cd347d6d
immortius <immortius@gmail.com>
2025-08-03 11:24:49 +1000

    refactor: added ModifierProvider interface for data components

--- src/main/java/com/wanderersoftherift/wotr/item/implicit/GearImplicits.java
+++ src/main/java/com/wanderersoftherift/wotr/item/implicit/GearImplicits.java
@@ -3,12 +3,17 @@ package com.wanderersoftherift.wotr.item.implicit;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.wanderersoftherift.wotr.modifier.ModifierInstance;
+import com.wanderersoftherift.wotr.modifier.ModifierProvider;
+import com.wanderersoftherift.wotr.modifier.WotrEquipmentSlot;
+import com.wanderersoftherift.wotr.modifier.source.GearImplicitModifierSource;
+import com.wanderersoftherift.wotr.modifier.source.ModifierSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 import java.util.List;
 
-public interface GearImplicits {
+public interface GearImplicits extends ModifierProvider {
     Codec<GearImplicits> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             ModifierInstance.CODEC.listOf()
                     .optionalFieldOf("instances", List.of())
@@ -26,4 +31,13 @@ public interface GearImplicits {
     List<ModifierInstance> modifierInstances(ItemStack stack, Level level);
 
     List<ModifierInstance> modifierInstances();
+
+    @Override
+    default void forEachModifier(ItemStack stack, WotrEquipmentSlot slot, LivingEntity entity, Visitor visitor) {
+        List<ModifierInstance> modifierInstances = modifierInstances(stack, entity.level());
+        for (ModifierInstance modifier : modifierInstances) {
+            ModifierSource source = new GearImplicitModifierSource(this, slot, entity);
+            visitor.accept(modifier.modifier(), modifier.tier(), modifier.roll(), source);
+        }
+    }
 }
--- src/main/java/com/wanderersoftherift/wotr/modifier/ModifierHelper.java
+++ src/main/java/com/wanderersoftherift/wotr/modifier/ModifierHelper.java
@@ -1,13 +1,5 @@
 package com.wanderersoftherift.wotr.modifier;
 
-import com.wanderersoftherift.wotr.init.WotrDataComponentType;
-import com.wanderersoftherift.wotr.item.implicit.GearImplicits;
-import com.wanderersoftherift.wotr.item.socket.GearSocket;
-import com.wanderersoftherift.wotr.item.socket.GearSockets;
-import com.wanderersoftherift.wotr.modifier.source.GearImplicitModifierSource;
-import com.wanderersoftherift.wotr.modifier.source.GearSocketModifierSource;
-import com.wanderersoftherift.wotr.modifier.source.ModifierSource;
-import net.minecraft.core.Holder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -23,50 +15,11 @@ public class ModifierHelper {
             ItemStack stack,
             WotrEquipmentSlot slot,
             LivingEntity entity,
-            ModifierInSlotVisitor visitor) {
-        if (!stack.isEmpty() && slot.canAccept(stack)) {
-            runOnImplicits(stack, slot, entity, visitor);
-            runOnGearSockets(stack, slot, entity, visitor);
-        }
+            ModifierProvider.Visitor visitor) {
+        stack.getAllOfType(ModifierProvider.class).forEach(x -> x.forEachModifier(stack, slot, entity, visitor));
     }
 
-    private static void runOnGearSockets(
-            ItemStack stack,
-            WotrEquipmentSlot slot,
-            LivingEntity entity,
-            ModifierInSlotVisitor visitor) {
-        GearSockets gearSockets = stack.get(WotrDataComponentType.GEAR_SOCKETS);
-        if (gearSockets != null && !gearSockets.isEmpty()) {
-            for (GearSocket socket : gearSockets.sockets()) {
-                if (socket.isEmpty()) {
-                    continue;
-                }
-                ModifierInstance modifierInstance = socket.modifier().get();
-                Holder<Modifier> modifier = modifierInstance.modifier();
-                if (modifier != null) {
-                    ModifierSource source = new GearSocketModifierSource(socket, gearSockets, slot, entity);
-                    visitor.accept(modifier, modifierInstance.tier(), modifierInstance.roll(), source);
-                }
-            }
-        }
-    }
-
-    private static void runOnImplicits(
-            ItemStack stack,
-            WotrEquipmentSlot slot,
-            LivingEntity entity,
-            ModifierInSlotVisitor visitor) {
-        GearImplicits implicits = stack.get(WotrDataComponentType.GEAR_IMPLICITS);
-        if (implicits != null) {
-            List<ModifierInstance> modifierInstances = implicits.modifierInstances(stack, entity.level());
-            for (ModifierInstance modifier : modifierInstances) {
-                ModifierSource source = new GearImplicitModifierSource(implicits, slot, entity);
-                visitor.accept(modifier.modifier(), modifier.tier(), modifier.roll(), source);
-            }
-        }
-    }
-
-    public static void runIterationOnEquipment(LivingEntity entity, ModifierInSlotVisitor visitor) {
+    public static void runIterationOnEquipment(LivingEntity entity, ModifierProvider.Visitor visitor) {
         var slots = NeoForge.EVENT_BUS.post(new CollectEquipmentSlotsEvent(new ArrayList<>(), entity)).getSlots();
         for (var wotrSlot : slots) {
             runIterationOnItem(wotrSlot.getContent(entity), wotrSlot, entity, visitor);
@@ -99,11 +52,6 @@ public class ModifierHelper {
     public static void disableModifier(ItemStack stack, LivingEntity entity, WotrEquipmentSlot slot) {
         runIterationOnItem(stack, slot, entity, (modifierHolder, tier, roll, source) -> modifierHolder.value()
                 .disableModifier(roll, entity, source, tier));
-    }
-
-    @FunctionalInterface
-    public interface ModifierInSlotVisitor {
-        void accept(Holder<Modifier> modifierHolder, int tier, float roll, ModifierSource item);
     }
 
     public static class CollectEquipmentSlotsEvent extends Event {
