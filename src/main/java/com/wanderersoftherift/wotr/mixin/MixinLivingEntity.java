81e524bf90406eef18a4a01f82072bab0af243c8
immortius <immortius@gmail.com>
2025-08-30 10:13:11 +1000

    refactor: remove unneeded mixin extends.

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
@@ -7,14 +7,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.common.NeoForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,11 +22,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Map;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity extends Entity {
-
-    public MixinLivingEntity(EntityType<?> entityType, Level level) {
-        super(entityType, level);
-    }
+public abstract class MixinLivingEntity {
 
     @Inject(method = "onChangedBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;runLocationChangedEffects(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;)V"))
     private void onChangedBlockRunLocationChangedEffects(ServerLevel level, BlockPos pos, CallbackInfo ci) {
@@ -58,7 +51,7 @@ public abstract class MixinLivingEntity extends Entity {
 
     @Inject(method = "onAttributeUpdated", at = @At(value = "TAIL"))
     private void sendPlayerEventOnAttributeUpdated(Holder<Attribute> attribute, CallbackInfo ci) {
-        LivingEntity entity = (LivingEntity) (Entity) this;
+        LivingEntity entity = (LivingEntity) (Object) this;
         if (entity instanceof ServerPlayer player) {
             NeoForge.EVENT_BUS.post(new PlayerAttributeChangedEvent(player, attribute));
         }
