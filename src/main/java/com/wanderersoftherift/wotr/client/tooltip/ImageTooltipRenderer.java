b219f1f882933309347545c98de2e6561f5f2741
ImplementsLegend <thsmpsns50@gmail.com>
2025-09-07 22:29:23 +0200

    fix: NPE in ImageTooltipRenderer

--- src/main/java/com/wanderersoftherift/wotr/client/tooltip/ImageTooltipRenderer.java
+++ src/main/java/com/wanderersoftherift/wotr/client/tooltip/ImageTooltipRenderer.java
@@ -31,6 +31,9 @@ public class ImageTooltipRenderer implements ClientTooltipComponent {
     }
 
     public static void renderImage(ImageComponent component, int x, int y, GuiGraphics guiGraphics) {
+        if (component.asset() == null) {
+            return;
+        }
         var textureWidth = TextureUtils.getTextureWidthGL(component.asset());
         var textureHeight = TextureUtils.getTextureHeightGL(component.asset());
         guiGraphics.blit(RenderType::guiTextured, component.asset(), x, y - 1, 0F, 0F, textureWidth, textureHeight,
@@ -45,17 +48,28 @@ public class ImageTooltipRenderer implements ClientTooltipComponent {
             Matrix4f pMatrix4f,
             MultiBufferSource.BufferSource pBufferSource,
             Function<Component, Component> textTransform) {
+        if (component.asset() == null) {
+            pFont.drawInBatch(textTransform.apply(component.base()), pX + 2, pY + 1, 0xAABBCC, true, pMatrix4f,
+                    pBufferSource, Font.DisplayMode.NORMAL, 0, 0x00f0_00f0);
+            return;
+        }
         var textureWidth = TextureUtils.getTextureWidthGL(component.asset());
         pFont.drawInBatch(textTransform.apply(component.base()), pX + textureWidth + 2, pY + 1, 0xAABBCC, true,
                 pMatrix4f, pBufferSource, Font.DisplayMode.NORMAL, 0, 0x00f0_00f0);
     }
 
     public static int getHeight(ImageComponent component, Font font) {
+        if (component.asset() == null) {
+            return font.lineHeight;
+        }
         var textureHeight = TextureUtils.getTextureHeightGL(component.asset());
-        return Math.max(textureHeight, Minecraft.getInstance().font.lineHeight);
+        return Math.max(textureHeight, font.lineHeight);
     }
 
     public static int getWidth(ImageComponent component, Font font) {
+        if (component.asset() == null) {
+            return Math.max(0, font.width(component.base().getString())) + 2;
+        }
         var textureWidth = TextureUtils.getTextureWidthGL(component.asset());
         return Math.max(0, font.width(component.base().getString()) + textureWidth) + 2;
     }
