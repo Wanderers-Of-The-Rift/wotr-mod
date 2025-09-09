ebcd8e2b3c441def72e42e505dc83cfb6561a7d1
ImplementsLegend <thsmpsns50@gmail.com>
2025-09-09 20:05:47 +0200

    changed TextureUtils to use outdated OpenGL api to improve compatibility

--- src/main/java/com/wanderersoftherift/wotr/util/TextureUtils.java
+++ src/main/java/com/wanderersoftherift/wotr/util/TextureUtils.java
@@ -1,8 +1,9 @@
 package com.wanderersoftherift.wotr.util;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
-import org.lwjgl.opengl.GL45;
+import org.lwjgl.opengl.GL33;
 
 public class TextureUtils {
 
@@ -13,8 +14,18 @@ public class TextureUtils {
      * @return The Image width as an int
      */
     public static int getTextureWidthGL(ResourceLocation resource) {
+        if (!RenderSystem.isOnRenderThread()) {
+            return -1;
+        }
         var id = Minecraft.getInstance().getTextureManager().getTexture(resource).getId();
-        return GL45.glGetTextureLevelParameteri(id, 0, GL45.GL_TEXTURE_WIDTH);
+        // return GL45.glGetTextureLevelParameteri(id, 0, GL45.GL_TEXTURE_WIDTH); // requires OpenGL 4.5
+        var current = GL33.glGetInteger(GL33.GL_TEXTURE_BINDING_2D);
+        try {
+            GL33.glBindTexture(GL33.GL_TEXTURE_2D, id);
+            return GL33.glGetTexLevelParameteri(GL33.GL_TEXTURE_2D, 0, GL33.GL_TEXTURE_WIDTH);
+        } finally {
+            GL33.glBindTexture(GL33.GL_TEXTURE_2D, current);
+        }
     }
 
     /**
@@ -24,8 +35,18 @@ public class TextureUtils {
      * @return The Image height as an int
      */
     public static int getTextureHeightGL(ResourceLocation resource) {
+        if (!RenderSystem.isOnRenderThread()) {
+            return -1;
+        }
         var id = Minecraft.getInstance().getTextureManager().getTexture(resource).getId();
-        return GL45.glGetTextureLevelParameteri(id, 0, GL45.GL_TEXTURE_HEIGHT);
+        // return GL45.glGetTextureLevelParameteri(id, 0, GL45.GL_TEXTURE_HEIGHT); // requires OpenGL 4.5
+        var current = GL33.glGetInteger(GL33.GL_TEXTURE_BINDING_2D);
+        try {
+            GL33.glBindTexture(GL33.GL_TEXTURE_2D, id);
+            return GL33.glGetTexLevelParameteri(GL33.GL_TEXTURE_2D, 0, GL33.GL_TEXTURE_HEIGHT);
+        } finally {
+            GL33.glBindTexture(GL33.GL_TEXTURE_2D, current);
+        }
     }
 
 }
