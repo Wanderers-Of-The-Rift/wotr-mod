e5b8c742b37db0d2de2f8ca22cbf83b43c64820e
attack8 <attackeight@gmail.com>
2025-09-10 14:10:41 -0400

    Remove Mixin

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinLivingEntity.java
@@ -1,11 +1,9 @@
 package com.wanderersoftherift.wotr.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.wanderersoftherift.wotr.core.rift.RiftLevelManager;
 import com.wanderersoftherift.wotr.modifier.ModifierHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
@@ -19,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Map;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity {
+public class MixinLivingEntity {
 
     @Inject(method = "onChangedBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;runLocationChangedEffects(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;)V"))
     private void onChangedBlockRunLocationChangedEffects(ServerLevel level, BlockPos pos, CallbackInfo ci) {
@@ -44,13 +42,6 @@ public abstract class MixinLivingEntity {
             CallbackInfo ci) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
         ModifierHelper.disableModifier(stack, livingEntity, slot);
-    }
-
-    @Inject(method = "dropAllDeathLoot", at = @At(value = "HEAD"), cancellable = true)
-    private void stopDeathLootInRift(ServerLevel sLevel, DamageSource damageSource, CallbackInfo ci) {
-        if (RiftLevelManager.isRift(sLevel)) {
-            ci.cancel();
-        }
     }
 
 }
