dacd2528ac4e6beabee2cd0eec31e0ed86550342
immortius <immortius@gmail.com>
2025-09-13 22:15:10 +1000

    refactor: AbilityTargeting is now an interface feat: Reworked existing targeting to work with updated targeting framework feat: Replaced self targeting with FilterTargeting to apply additional filtering to an existing target set feat: Added radial targeting for spherical area targeting feat: Improved raycast targeting feat: CasterTargeting for retargeting back feat: test crater skill fix: fixed some of the loose ends in simple projectils feat: support for block piercing and lifespan configuration for simple projectiles feat: simple "all" and "none" options for TargetEntityPredicate feat: skeleton of a TargetBlockPredicate

--- src/main/java/com/wanderersoftherift/wotr/abilities/targeting/AbilityTargeting.java
+++ src/main/java/com/wanderersoftherift/wotr/abilities/targeting/AbilityTargeting.java
@@ -1,101 +1,24 @@
 package com.wanderersoftherift.wotr.abilities.targeting;
 
-import com.mojang.datafixers.Products;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.wanderersoftherift.wotr.abilities.AbilityContext;
-import com.wanderersoftherift.wotr.abilities.effects.predicate.TargetPredicate;
 import com.wanderersoftherift.wotr.init.WotrRegistries;
 import com.wanderersoftherift.wotr.modifier.effect.ModifierEffect;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public abstract class AbilityTargeting {
+public interface AbilityTargeting {
 
-    public static final Codec<AbilityTargeting> DIRECT_CODEC = WotrRegistries.EFFECT_TARGETING_TYPES.byNameCodec()
+    Codec<AbilityTargeting> DIRECT_CODEC = WotrRegistries.EFFECT_TARGETING_TYPES.byNameCodec()
             .dispatch(AbilityTargeting::getCodec, Function.identity());
 
-    private final TargetPredicate targetPredicate;
+    MapCodec<? extends AbilityTargeting> getCodec();
 
-    public AbilityTargeting(TargetPredicate targetPredicate) {
-        this.targetPredicate = targetPredicate;
-    }
+    List<TargetInfo> getTargets(AbilityContext context, TargetInfo origin);
 
-    public abstract MapCodec<? extends AbilityTargeting> getCodec();
-
-    public List<TargetInfo> getTargets(AbilityContext context, TargetInfo origin) {
-        List<TargetInfo> results = new ArrayList<>();
-
-        for (HitResult source : origin.targets()) {
-            List<HitResult> hits = new ArrayList<>();
-            if (source instanceof EntityHitResult entityHit) {
-                hits.addAll(getTargetsFromEntity(entityHit.getEntity(), context).stream()
-                        .map(x -> new EntityHitResult(x, x.position()))
-                        .toList());
-            } else if (source instanceof BlockHitResult blockHit) {
-                hits.addAll(getTargetsFromBlocks(List.of(blockHit.getBlockPos()), context).stream()
-                        .map(x -> new EntityHitResult(x, x.position()))
-                        .toList());
-            }
-            results.add(new TargetInfo(source, hits));
-        }
-        return results;
-    }
-
-    /**
-     * @param currentEntity This is the entity which is using the effect, this can be any entity down a chain based on
-     *                      the effect list, this determines the location around where the effect is targeting
-     * @param blocks        A list of blocks which can be a point of reference for targeting enemies around them. This
-     *                      is mainly used for raycasting based effects
-     * @param context       Context of the effect
-     * @return The list of entities selected by the targeting method.
-     */
-    public List<Entity> getTargets(Entity currentEntity, List<BlockPos> blocks, AbilityContext context) {
-        List<Entity> targets = new ArrayList<>();
-        if (currentEntity != null) {
-            targets.addAll(getTargetsFromEntity(currentEntity, context));
-        } else {
-            targets.addAll(getTargetsFromBlocks(blocks, context));
-        }
-
-        return targets;
-    }
-
-    protected List<Entity> getTargetsFromEntity(Entity entity, AbilityContext context) {
-        return new ArrayList<>();
-    }
-
-    protected List<Entity> getTargetsFromBlocks(List<BlockPos> blocks, AbilityContext context) {
-        return new ArrayList<>();
-    }
-
-    public List<BlockPos> getBlocks(Entity user) {
-        return new ArrayList<>();
-    }
-
-    public List<BlockPos> getBlocksInArea(Entity entity, List<BlockPos> targetPos, AbilityContext context) {
-        return new ArrayList<>();
-    }
-
-    public boolean isRelevant(ModifierEffect modifierEffect) {
+    default boolean isRelevant(ModifierEffect modifierEffect) {
         return false;
-    }
-
-    public TargetPredicate getTargetPredicate() {
-        return targetPredicate;
-    }
-
-    protected static <T extends AbilityTargeting> Products.P1<RecordCodecBuilder.Mu<T>, TargetPredicate> commonFields(
-            RecordCodecBuilder.Instance<T> instance) {
-        return instance.group(TargetPredicate.CODEC.optionalFieldOf("target", new TargetPredicate())
-                .forGetter(AbilityTargeting::getTargetPredicate));
     }
 }
