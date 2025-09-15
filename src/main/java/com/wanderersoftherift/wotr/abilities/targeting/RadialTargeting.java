80e1ff295491a400926c9de59d7441e1844dd921
immortius <immortius@gmail.com>
2025-09-16 08:25:08 +1000

    refactor: pull out common area targeting feat: set block effect

--- src/main/java/com/wanderersoftherift/wotr/abilities/targeting/RadialTargeting.java
+++ src/main/java/com/wanderersoftherift/wotr/abilities/targeting/RadialTargeting.java
@@ -1,47 +1,21 @@
 package com.wanderersoftherift.wotr.abilities.targeting;
 
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import com.wanderersoftherift.wotr.abilities.AbilityContext;
 import com.wanderersoftherift.wotr.abilities.effects.predicate.TargetBlockPredicate;
 import com.wanderersoftherift.wotr.abilities.effects.predicate.TargetEntityPredicate;
-import com.wanderersoftherift.wotr.init.WotrAttributes;
-import com.wanderersoftherift.wotr.modifier.effect.AttributeModifierEffect;
-import com.wanderersoftherift.wotr.modifier.effect.ModifierEffect;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class RadialTargeting implements AbilityTargeting {
+public class RadialTargeting extends AreaTargeting {
     public static final MapCodec<RadialTargeting> CODEC = RecordCodecBuilder.mapCodec(
-            instance -> instance.group(
-                    TargetEntityPredicate.CODEC.optionalFieldOf("entities", TargetEntityPredicate.ALL)
-                            .forGetter(RadialTargeting::getEntityPredicate),
-                    TargetBlockPredicate.CODEC.optionalFieldOf("blocks", TargetBlockPredicate.NONE)
-                            .forGetter(RadialTargeting::getBlockPredicate),
-                    Codec.FLOAT.fieldOf("range").forGetter(RadialTargeting::getRange),
-                    Codec.BOOL.optionalFieldOf("include_self", true).forGetter(RadialTargeting::getIncludeSelf)
-            ).apply(instance, RadialTargeting::new));
-
-    private final TargetEntityPredicate entityPredicate;
-    private final TargetBlockPredicate blockPredicate;
-    private final float range;
-    private final boolean includeSelf;
+            instance -> commonFields(instance).apply(instance, RadialTargeting::new));
 
     public RadialTargeting(TargetEntityPredicate entities, TargetBlockPredicate blocks, float range,
-            boolean includeSelf) {
-        this.entityPredicate = entities;
-        this.blockPredicate = blocks;
-        this.range = range;
-        this.includeSelf = includeSelf;
+            boolean includeSelf, boolean alignToBlock) {
+        super(entities, blocks, range, includeSelf, alignToBlock);
     }
 
     @Override
@@ -50,72 +24,20 @@ public class RadialTargeting implements AbilityTargeting {
     }
 
     @Override
-    public List<TargetInfo> getTargets(AbilityContext context, TargetInfo origin) {
-        float finalRange = getRange(context);
-        float finalRangeSqr = finalRange * finalRange;
-
-        List<TargetInfo> result = new ArrayList<>();
-        for (HitResult source : origin.targets()) {
-            List<HitResult> hits = new ArrayList<>();
-            AABB aabb = AABB.ofSize(source.getLocation(), 2 * finalRange, 2 * finalRange, 2 * finalRange);
-            if (entityPredicate != TargetEntityPredicate.NONE) {
-                Entity sourceEntity;
-                if (source instanceof EntityHitResult sourceEntityHit) {
-                    sourceEntity = sourceEntityHit.getEntity();
-                } else {
-                    sourceEntity = null;
-                }
-                hits.addAll(context.level()
-                        .getEntities((Entity) null, aabb,
-                                (target) -> target.distanceToSqr(source.getLocation()) < finalRangeSqr
-                                        && entityPredicate.matches(target, context.caster())
-                                        && (includeSelf || target != sourceEntity))
-                        .stream()
-                        .map(entity -> new EntityHitResult(entity, entity.position()))
-                        .toList());
-            }
-            if (blockPredicate != TargetBlockPredicate.NONE) {
-                for (BlockPos pos : BlockPos.betweenClosed(aabb)) {
-                    if (pos.closerToCenterThan(source.getLocation(), finalRange)
-                            && blockPredicate.matches(pos, context.level())) {
-                        // TODO: review isInside
-                        hits.add(new BlockHitResult(pos.getCenter(),
-                                Direction.getApproximateNearest(pos.getCenter().subtract(source.getLocation())),
-                                new BlockPos(pos), false));
-                    }
-                }
-            }
-            if (!hits.isEmpty()) {
-                result.add(new TargetInfo(source, hits));
-            }
+    protected AABB getArea(HitResult source, float range) {
+        if (getAlignToBlock()) {
+            return AABB.ofSize(BlockPos.containing(source.getLocation()).getCenter(), 2 * range, 2 * range, 2 * range);
         }
-
-        return result;
+        return AABB.ofSize(source.getLocation(), 2 * range, 2 * range, 2 * range);
     }
 
     @Override
-    public boolean isRelevant(ModifierEffect modifierEffect) {
-        return modifierEffect instanceof AttributeModifierEffect attributeModifierEffect
-                && WotrAttributes.ABILITY_AOE.equals(attributeModifierEffect.attribute());
+    protected boolean inArea(Vec3 center, Vec3 pos, float range) {
+        return pos.distanceToSqr(center) < range * range;
     }
 
-    private TargetEntityPredicate getEntityPredicate() {
-        return entityPredicate;
-    }
-
-    public TargetBlockPredicate getBlockPredicate() {
-        return blockPredicate;
-    }
-
-    private float getRange() {
-        return range;
-    }
-
-    private float getRange(AbilityContext context) {
-        return context.getAbilityAttribute(WotrAttributes.ABILITY_AOE, range);
-    }
-
-    private boolean getIncludeSelf() {
-        return includeSelf;
+    @Override
+    protected boolean inArea(Vec3 center, BlockPos pos, float range) {
+        return pos.closerToCenterThan(center, range);
     }
 }
