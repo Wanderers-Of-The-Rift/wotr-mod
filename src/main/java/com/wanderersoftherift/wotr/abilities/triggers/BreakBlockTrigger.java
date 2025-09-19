3dea11cdac9e7c5883114b95485920070da82e76
immortius <immortius@gmail.com>
2025-09-19 20:39:04 +1000

    feat: Capture target from triggers as an ability context data component feat: Trigger target targeting feat: Connected block targeting feat: Veinminer ability

--- src/main/java/com/wanderersoftherift/wotr/abilities/triggers/BreakBlockTrigger.java
+++ src/main/java/com/wanderersoftherift/wotr/abilities/triggers/BreakBlockTrigger.java
@@ -4,12 +4,17 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.wanderersoftherift.wotr.abilities.AbilityContext;
 import com.wanderersoftherift.wotr.abilities.TrackedAbilityTrigger;
+import com.wanderersoftherift.wotr.abilities.attachment.TargetComponent;
+import com.wanderersoftherift.wotr.init.WotrDataComponentType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.BlockHitResult;
 
-public record BreakBlockTrigger(BlockPos pos) implements TrackedAbilityTrigger {
+public record BreakBlockTrigger(BlockPos pos, Direction dir) implements TrackedAbilityTrigger {
 
     public static final MapCodec<BreakBlockTrigger> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            BlockPos.CODEC.fieldOf("pos").forGetter(BreakBlockTrigger::pos)
+            BlockPos.CODEC.fieldOf("pos").forGetter(BreakBlockTrigger::pos),
+            Direction.CODEC.fieldOf("direction").forGetter(BreakBlockTrigger::dir)
     ).apply(instance, BreakBlockTrigger::new));
 
     public static final TriggerType<BreakBlockTrigger> TYPE = new TriggerType<>(CODEC, null);
@@ -21,6 +26,7 @@ public record BreakBlockTrigger(BlockPos pos) implements TrackedAbilityTrigger {
 
     @Override
     public void addComponents(AbilityContext context) {
-
+        context.set(WotrDataComponentType.AbilityContextData.TRIGGER_TARGET,
+                new TargetComponent(new BlockHitResult(pos.getCenter(), dir, pos, false)));
     }
 }
