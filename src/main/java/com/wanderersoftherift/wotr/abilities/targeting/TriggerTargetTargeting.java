06c720a6daff5a531f0ce118ddf2ad6629611910
immortius <immortius@gmail.com>
2025-09-19 20:39:04 +1000

    feat: Capture target from triggers as an ability context data component feat: Trigger target targeting feat: Connected block targeting feat: Veinminer ability

---
+++ src/main/java/com/wanderersoftherift/wotr/abilities/targeting/TriggerTargetTargeting.java
@@ -0,0 +1,34 @@
+package com.wanderersoftherift.wotr.abilities.targeting;
+
+import com.mojang.serialization.MapCodec;
+import com.wanderersoftherift.wotr.abilities.AbilityContext;
+import com.wanderersoftherift.wotr.abilities.attachment.TargetComponent;
+import com.wanderersoftherift.wotr.init.WotrDataComponentType;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.phys.HitResult;
+
+import java.util.List;
+
+public final class TriggerTargetTargeting implements AbilityTargeting {
+
+    public static final TriggerTargetTargeting INSTANCE = new TriggerTargetTargeting();
+    public static final MapCodec<TriggerTargetTargeting> CODEC = MapCodec.unit(INSTANCE);
+
+    private TriggerTargetTargeting() {
+    }
+
+    @Override
+    public MapCodec<? extends AbilityTargeting> getCodec() {
+        return CODEC;
+    }
+
+    @Override
+    public List<TargetInfo> getTargets(AbilityContext context, TargetInfo origin) {
+        TargetComponent triggerSource = context.get(WotrDataComponentType.AbilityContextData.TRIGGER_TARGET);
+        if (triggerSource == null) {
+            return List.of();
+        }
+        HitResult target = triggerSource.asHitResult((ServerLevel) context.level());
+        return List.of(new TargetInfo(origin.source(), List.of(target)));
+    }
+}
