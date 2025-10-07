d6e11c5c2fcbf088ccd089e1dbcb5c11dcda7bd3
Jutta <jutta.k.lehtonen@gmail.com>
2025-10-08 00:16:53 +0300

    feat: pick item to collect from wooden chest loot table, exclude non-stackable items

--- src/main/java/com/wanderersoftherift/wotr/rift/objective/definition/CollectObjective.java
+++ src/main/java/com/wanderersoftherift/wotr/rift/objective/definition/CollectObjective.java
@@ -6,25 +6,52 @@ import com.wanderersoftherift.wotr.rift.objective.ObjectiveType;
 import com.wanderersoftherift.wotr.rift.objective.OngoingObjective;
 import com.wanderersoftherift.wotr.rift.objective.ongoing.CollectOngoingObjective;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.storage.loot.LootTable;
+
+import java.util.Optional;
 
 public class CollectObjective implements ObjectiveType {
+
     public static final MapCodec<CollectObjective> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
             ExtraCodecs.POSITIVE_INT.fieldOf("min_quantity").forGetter(CollectObjective::getMinQuantity),
             ExtraCodecs.POSITIVE_INT.fieldOf("max_quantity").forGetter(CollectObjective::getMaxQuantity),
-            BuiltInRegistries.ITEM.byNameCodec().fieldOf("item").forGetter(CollectObjective::getItem)
-    ).apply(inst, CollectObjective::new));
+
+            BuiltInRegistries.ITEM.byNameCodec()
+                    .optionalFieldOf("item")
+                    .forGetter(o -> Optional.ofNullable(o.getItem())),
+
+            ResourceLocation.CODEC.optionalFieldOf("loot_table")
+                    .forGetter(o -> Optional.ofNullable(o.getLootTable()).map(ResourceKey::location))
+
+    ).apply(inst, (minQ, maxQ, optItem, optLootRl) -> {
+        Item item = optItem.orElse(null);
+        ResourceKey<LootTable> lootKey = optLootRl.map(rl -> ResourceKey.create(Registries.LOOT_TABLE, rl))
+                .orElse(null);
+        return new CollectObjective(minQ, maxQ, item, lootKey);
+    }));
 
     private final int minQuantity;
     private final int maxQuantity;
     private final Item item;
+    private final ResourceKey<LootTable> lootTable;
 
     public CollectObjective(int minQuantity, int maxQuantity, Item item) {
+        this(minQuantity, maxQuantity, item, null);
+    }
+
+    public CollectObjective(int minQuantity, int maxQuantity, Item item, ResourceKey<LootTable> lootTable) {
         this.minQuantity = minQuantity;
         this.maxQuantity = maxQuantity;
         this.item = item;
+        this.lootTable = lootTable;
     }
 
     @Override
@@ -35,7 +62,12 @@ public class CollectObjective implements ObjectiveType {
     @Override
     public OngoingObjective generate(LevelAccessor level) {
         int target = level.getRandom().nextIntBetweenInclusive(minQuantity, maxQuantity);
-        return new CollectOngoingObjective(target, item);
+
+        if (lootTable != null && level instanceof ServerLevel sl) {
+            Item picked = CollectOngoingObjective.pickItemFromLootOrThrow(sl, lootTable);
+            return new CollectOngoingObjective(target, picked);
+        }
+        return new CollectOngoingObjective(target, item != null ? item : Items.IRON_INGOT);
     }
 
     public int getMinQuantity() {
@@ -48,5 +80,9 @@ public class CollectObjective implements ObjectiveType {
 
     public Item getItem() {
         return item;
+    }
+
+    public ResourceKey<LootTable> getLootTable() {
+        return lootTable;
     }
 }
