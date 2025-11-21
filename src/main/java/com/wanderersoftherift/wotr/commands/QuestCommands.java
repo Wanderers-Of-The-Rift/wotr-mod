cea1f0f823877ee64d8f22eb861f2a958f2bfbe6
immortius <immortius@gmail.com>
2025-11-21 19:56:22 +1100

    feat: #424 rework quest commands to all resetting quests from individual npcs feat: add command for setting quest completion counts

--- src/main/java/com/wanderersoftherift/wotr/commands/QuestCommands.java
+++ src/main/java/com/wanderersoftherift/wotr/commands/QuestCommands.java
@@ -1,16 +1,21 @@
 package com.wanderersoftherift.wotr.commands;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import com.wanderersoftherift.wotr.core.npc.NpcIdentity;
 import com.wanderersoftherift.wotr.core.quest.Quest;
 import com.wanderersoftherift.wotr.core.quest.QuestLog;
 import com.wanderersoftherift.wotr.init.WotrAttachments;
+import com.wanderersoftherift.wotr.init.WotrRegistries;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.arguments.ResourceArgument;
+import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -30,21 +35,55 @@ public class QuestCommands extends BaseCommand {
 
     @Override
     protected void buildCommand(LiteralArgumentBuilder<CommandSourceStack> builder, CommandBuildContext context) {
-        String targetArg = "target";
+        String playerArg = "player";
+        String npcArg = "npc";
+        String questArg = "quest";
+        String amountArg = "amount";
         builder.then(Commands.literal("log")
                 .then(Commands.literal("list")
                         .executes(ctx -> printLog(ctx, ctx.getSource().getPlayerOrException()))
-                        .then(Commands.argument(targetArg, EntityArgument.player())
-                                .executes(ctx -> printLog(ctx, EntityArgument.getPlayer(ctx, targetArg)))))
+                        .then(Commands.argument(playerArg, EntityArgument.player())
+                                .executes(ctx -> printLog(ctx, EntityArgument.getPlayer(ctx, playerArg)))))
+                .then(Commands.literal("set")
+                        .then(Commands
+                                .argument(questArg, ResourceArgument.resource(context, WotrRegistries.Keys.QUESTS))
+                                .then(Commands.argument(amountArg, IntegerArgumentType.integer(0))
+                                        .executes(ctx -> setLog(ctx,
+                                                ResourceArgument.getResource(ctx, questArg, WotrRegistries.Keys.QUESTS),
+                                                IntegerArgumentType.getInteger(ctx, amountArg),
+                                                ctx.getSource().getPlayerOrException()))
+                                        .then(Commands.argument(playerArg, EntityArgument.player())
+                                                .executes(ctx -> setLog(ctx,
+                                                        ResourceArgument.getResource(ctx, questArg,
+                                                                WotrRegistries.Keys.QUESTS),
+                                                        IntegerArgumentType.getInteger(ctx, amountArg),
+                                                        EntityArgument.getPlayer(ctx, playerArg)))))))
                 .then(Commands.literal("reset")
                         .executes(ctx -> resetLog(ctx, ctx.getSource().getPlayerOrException()))
-                        .then(Commands.argument(targetArg, EntityArgument.player())
-                                .executes(ctx -> resetLog(ctx, EntityArgument.getPlayer(ctx, targetArg))))));
+                        .then(Commands.argument(playerArg, EntityArgument.player())
+                                .executes(ctx -> resetLog(ctx, EntityArgument.getPlayer(ctx, playerArg))))));
         builder.then(Commands.literal("available")
                 .then(Commands.literal("reset")
-                        .executes(ctx -> resetAvailable(ctx, ctx.getSource().getPlayerOrException()))
-                        .then(Commands.argument(targetArg, EntityArgument.player())
-                                .executes(ctx -> resetAvailable(ctx, EntityArgument.getPlayer(ctx, targetArg))))));
+                        .then(Commands.literal("all")
+                                .executes(ctx -> resetAllAvailable(ctx, ctx.getSource().getPlayerOrException()))
+                                .then(Commands.argument(playerArg, EntityArgument.player())
+                                        .executes(ctx -> resetAllAvailable(ctx,
+                                                EntityArgument.getPlayer(ctx, playerArg)))))
+                        .then(Commands.argument(npcArg, ResourceArgument.resource(context, WotrRegistries.Keys.NPCS))
+                                .executes(ctx -> resetAvailable(ctx,
+                                        ResourceArgument.getResource(ctx, npcArg, WotrRegistries.Keys.NPCS),
+                                        ctx.getSource().getPlayerOrException()))
+                                .then(Commands.argument(playerArg, EntityArgument.player())
+                                        .executes(ctx -> resetAvailable(ctx,
+                                                ResourceArgument.getResource(ctx, npcArg, WotrRegistries.Keys.NPCS),
+                                                EntityArgument.getPlayer(ctx, playerArg)))))));
+    }
+
+    private int setLog(CommandContext<CommandSourceStack> ctx, Holder<Quest> quest, int amount, ServerPlayer player) {
+        QuestLog questLog = player.getData(WotrAttachments.QUEST_LOG);
+        questLog.setCompletionCount(quest, amount);
+        ctx.getSource().sendSystemMessage(SUCCESS);
+        return Command.SINGLE_SUCCESS;
     }
 
     private int resetLog(CommandContext<CommandSourceStack> ctx, ServerPlayer player) {
@@ -53,8 +92,14 @@ public class QuestCommands extends BaseCommand {
         return Command.SINGLE_SUCCESS;
     }
 
-    private int resetAvailable(CommandContext<CommandSourceStack> ctx, ServerPlayer player) {
+    private int resetAllAvailable(CommandContext<CommandSourceStack> ctx, ServerPlayer player) {
         player.removeData(WotrAttachments.AVAILABLE_QUESTS);
+        ctx.getSource().sendSystemMessage(SUCCESS);
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private int resetAvailable(CommandContext<CommandSourceStack> ctx, Holder<NpcIdentity> npc, ServerPlayer player) {
+        player.getExistingData(WotrAttachments.AVAILABLE_QUESTS).ifPresent(quests -> quests.clearQuests(npc));
         ctx.getSource().sendSystemMessage(SUCCESS);
         return Command.SINGLE_SUCCESS;
     }
@@ -64,9 +109,9 @@ public class QuestCommands extends BaseCommand {
         ctx.getSource().sendSystemMessage(PRINT_LOG);
         data.getQuestCounts().forEach((quest, count) -> {
             ctx.getSource()
-                    .sendSystemMessage(Component.literal(" ")
-                            .append(Quest.title(quest))
-                            .append(": ")
+                    .sendSystemMessage(Component.literal(" \"")
+                            .append(quest.getRegisteredName())
+                            .append("\": ")
                             .append(Integer.toString(count)));
         });
         return Command.SINGLE_SUCCESS;
