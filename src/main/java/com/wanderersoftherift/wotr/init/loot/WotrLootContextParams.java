2ac37287fb11f7bf814c85f6003bbe022d315c72
immortius <immortius@gmail.com>
2025-11-26 21:30:38 +1100

    feat: bridge rift parameters to loot parameters feat: replace kill objective with goal based version

--- src/main/java/com/wanderersoftherift/wotr/init/loot/WotrLootContextParams.java
+++ src/main/java/com/wanderersoftherift/wotr/init/loot/WotrLootContextParams.java
@@ -1,13 +1,14 @@
 package com.wanderersoftherift.wotr.init.loot;
 
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
-import com.wanderersoftherift.wotr.core.rift.RiftConfig;
+import com.wanderersoftherift.wotr.core.rift.parameter.RiftParameterData;
 import net.minecraft.util.context.ContextKey;
 
 public final class WotrLootContextParams {
 
     public static final ContextKey<Integer> RIFT_TIER = new ContextKey<>(WanderersOfTheRift.id("rift_tier"));
-    public static final ContextKey<RiftConfig> RIFT_CONFIG = new ContextKey<>(WanderersOfTheRift.id("rift_config"));
+    public static final ContextKey<RiftParameterData> RIFT_PARAMETERS = new ContextKey<>(
+            WanderersOfTheRift.id("rift_parameters"));
 
     private WotrLootContextParams() {
     }
