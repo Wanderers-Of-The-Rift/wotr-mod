7a9393d23e3a35cbce6aa72a2957b5dd304e1fe6
immortius <immortius@gmail.com>
2025-11-29 15:59:10 +1100

    refactor: goal tracking handling

--- src/main/java/com/wanderersoftherift/wotr/core/goal/GoalEventHandler.java
+++ src/main/java/com/wanderersoftherift/wotr/core/goal/GoalEventHandler.java
@@ -5,18 +5,12 @@ import com.wanderersoftherift.wotr.core.goal.type.CloseAnomalyGoal;
 import com.wanderersoftherift.wotr.core.goal.type.CompleteRiftGoal;
 import com.wanderersoftherift.wotr.core.goal.type.KillMobGoal;
 import com.wanderersoftherift.wotr.core.goal.type.RiftCompletionLevel;
-import com.wanderersoftherift.wotr.core.quest.ActiveQuests;
-import com.wanderersoftherift.wotr.core.rift.RiftData;
 import com.wanderersoftherift.wotr.core.rift.RiftEvent;
-import com.wanderersoftherift.wotr.core.rift.RiftLevelManager;
-import com.wanderersoftherift.wotr.init.WotrAttachments;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
-
-import java.util.function.Function;
 
 /**
  * Event handlers for supporting goals
@@ -24,36 +18,20 @@ import java.util.function.Function;
 @EventBusSubscriber
 public class GoalEventHandler {
 
-    // TODO: Find a better home?
-    // TODO: Change to an event supporting extension.
-    public static <T extends Goal> void updateGoal(
-            Player player,
-            Class<T> goalType,
-            Function<T, Integer> progressFunction) {
-        ActiveQuests activeQuests = player.getData(WotrAttachments.ACTIVE_QUESTS.get());
-        if (!(player.level() instanceof ServerLevel level)) {
-            return;
-        }
-        activeQuests.progressGoals(goalType, progressFunction, level);
-        if (RiftLevelManager.isRift(level)) {
-            RiftData data = RiftData.get(level);
-            data.progressGoals(goalType, progressFunction, level);
-        }
-    }
-
     @SubscribeEvent
     public static void onDiedInRift(RiftEvent.PlayerDied event) {
-        updateGoal(event.getPlayer(), CompleteRiftGoal.class, (goal) -> {
+        // TODO: Rather than sending an event, have a registry of GoalTracking providers? Pros, cons
+        NeoForge.EVENT_BUS.post(new GoalEvent.Update<>(event.getPlayer(), CompleteRiftGoal.class, (goal) -> {
             if (goal.completionLevel() == RiftCompletionLevel.ATTEMPT && goal.predicate().matches(event.getConfig())) {
                 return 1;
             }
             return 0;
-        });
+        }));
     }
 
     @SubscribeEvent
     public static void onCompletedRift(RiftEvent.PlayerCompletedRift event) {
-        updateGoal(event.getPlayer(), CompleteRiftGoal.class, (goal) -> {
+        NeoForge.EVENT_BUS.post(new GoalEvent.Update<>(event.getPlayer(), CompleteRiftGoal.class, (goal) -> {
             if (!event.isObjectiveComplete() && goal.completionLevel() == RiftCompletionLevel.COMPLETE) {
                 return 0;
             }
@@ -61,7 +39,7 @@ public class GoalEventHandler {
                 return 1;
             }
             return 0;
-        });
+        }));
     }
 
     @SubscribeEvent
@@ -69,22 +47,22 @@ public class GoalEventHandler {
         if (!(event.getSource().getEntity() instanceof Player player)) {
             return;
         }
-        updateGoal(player, KillMobGoal.class, (goal) -> {
+        NeoForge.EVENT_BUS.post(new GoalEvent.Update<>(player, KillMobGoal.class, (goal) -> {
             if (goal.mob().map(predicate -> predicate.matches(event.getEntity().getType())).orElse(true)) {
                 return 1;
             }
             return 0;
-        });
+        }));
     }
 
     @SubscribeEvent
     public static void onAnomalyClosed(AnomalyEvent.Closed event) {
-        updateGoal(event.getClosingPlayer(), CloseAnomalyGoal.class, (goal) -> {
+        NeoForge.EVENT_BUS.post(new GoalEvent.Update<>(event.getClosingPlayer(), CloseAnomalyGoal.class, (goal) -> {
             if (goal.anomalyType().map(type -> type.value().equals(event.getTaskType())).orElse(true)) {
                 return 1;
             }
             return 0;
-        });
+        }));
     }
 
 }
