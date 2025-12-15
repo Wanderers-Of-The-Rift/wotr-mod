21c4d18a044dd3d39b16167b5a9a6bcea06ff367
immortius <immortius@gmail.com>
2025-12-15 20:06:22 +1100

    feat: add ConnectedRoomIterator refactor: reduced duplicate code refactor: improved some naming

--- src/main/java/com/wanderersoftherift/wotr/loot/provider/number/RiftRoomCountNumberProvider.java
+++ src/main/java/com/wanderersoftherift/wotr/loot/provider/number/RiftRoomCountNumberProvider.java
@@ -2,15 +2,8 @@ package com.wanderersoftherift.wotr.loot.provider.number;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import com.wanderersoftherift.wotr.entity.portal.PortalSpawnLocation;
 import com.wanderersoftherift.wotr.init.WotrNumberProviders;
-import com.wanderersoftherift.wotr.world.level.FastRiftGenerator;
-import com.wanderersoftherift.wotr.world.level.levelgen.layout.RiftLayout;
-import com.wanderersoftherift.wotr.world.level.levelgen.space.RiftSpace;
-import com.wanderersoftherift.wotr.world.level.levelgen.space.RoomRiftSpace;
-import net.minecraft.core.SectionPos;
-import net.minecraft.core.Vec3i;
-import net.minecraft.server.level.ServerLevel;
+import com.wanderersoftherift.wotr.world.level.ConnectedRoomIterator;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.LootNumberProviderType;
@@ -18,49 +11,24 @@ import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
 import net.minecraft.world.level.storage.loot.providers.number.NumberProviders;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public record RiftRoomCountNumberProvider(NumberProvider roomDistance) implements NumberProvider {
+public record RiftRoomCountNumberProvider(NumberProvider roomDepth) implements NumberProvider {
 
     public static final MapCodec<RiftRoomCountNumberProvider> CODEC = RecordCodecBuilder
             .mapCodec(instance -> instance.group(
-                    NumberProviders.CODEC.optionalFieldOf("max_distance", ConstantValue.exactly(100))
-                            .forGetter(RiftRoomCountNumberProvider::roomDistance)
+                    NumberProviders.CODEC.optionalFieldOf("room_depth", ConstantValue.exactly(100))
+                            .forGetter(RiftRoomCountNumberProvider::roomDepth)
             ).apply(instance, RiftRoomCountNumberProvider::new));
 
     @Override
     public float getFloat(LootContext lootContext) {
-        ServerLevel level = lootContext.getLevel();
-        if (!(level.getChunkSource().getGenerator() instanceof FastRiftGenerator generator)) {
-            return 0;
+        var roomIterator = ConnectedRoomIterator.create(lootContext.getLevel(), roomDepth.getInt(lootContext));
+        int count = 0;
+        while (roomIterator.hasNext()) {
+            // TODO: add room predicate
+            roomIterator.next();
+            count++;
         }
-        RiftLayout layout = generator.getOrCreateLayout(level.getServer());
-
-        RiftSpace startRoom = layout.getChunkSpace(SectionPos.of(PortalSpawnLocation.DEFAULT_RIFT_EXIT_POSITION));
-        Set<Vec3i> connected = new HashSet<>();
-        connected.add(startRoom.origin());
-        List<RiftSpace> layerRooms = new ArrayList<>();
-        layerRooms.add(startRoom);
-
-        int distance = Math.max(1, roomDistance().getInt(lootContext));
-        for (int layer = 0; !layerRooms.isEmpty() && layer < distance - 1; layer++) {
-            List<RiftSpace> nextLayerRooms = new ArrayList<>(layerRooms.size());
-            for (RiftSpace space : layerRooms) {
-                if (space instanceof RoomRiftSpace room) {
-                    room.corridors().forEach(corridor -> {
-                        RiftSpace adjSpace = layout.getChunkSpace(corridor.getConnectingPos(room));
-                        if (adjSpace instanceof RoomRiftSpace && connected.add(adjSpace.origin())) {
-                            nextLayerRooms.add(adjSpace);
-                        }
-                    });
-                }
-            }
-            layerRooms = nextLayerRooms;
-        }
-        return connected.size() - 1; // Excluding the portal room
+        return count - 1; // Excluding the portal room
     }
 
     @Override
---
+++ src/main/java/com/wanderersoftherift/wotr/world/level/ConnectedRoomIterator.java
@@ -0,0 +1,128 @@
+package com.wanderersoftherift.wotr.world.level;
+
+import com.wanderersoftherift.wotr.entity.portal.PortalSpawnLocation;
+import com.wanderersoftherift.wotr.world.level.levelgen.layout.RiftLayout;
+import com.wanderersoftherift.wotr.world.level.levelgen.space.RiftSpace;
+import com.wanderersoftherift.wotr.world.level.levelgen.space.RoomRiftSpace;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.server.level.ServerLevel;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Iterator over rift rooms connected to an origin room
+ */
+public class ConnectedRoomIterator implements Iterator<RoomRiftSpace> {
+
+    private static final int DEFAULT_MAX_DEPTH = 10;
+
+    private final int maxDepth;
+    private final RiftLayout layout;
+
+    private final Set<Vec3i> visitedRooms = new HashSet<>();
+    private Deque<RoomRiftSpace> currentLayer = new ArrayDeque<>();
+    private Deque<RoomRiftSpace> nextLayer = new ArrayDeque<>();
+    private int depth = 0;
+
+    private ConnectedRoomIterator(RiftLayout layout, SectionPos start, int maxDepth) {
+        this.layout = layout;
+        this.maxDepth = maxDepth;
+        if ((layout.getChunkSpace(start) instanceof RoomRiftSpace startRoom)) {
+            visitedRooms.add(startRoom.origin());
+            currentLayer.add(startRoom);
+        }
+    }
+
+    /**
+     * @param level The level to iterate rooms in
+     * @return An iterator over the rooms in the level, from the portal room up to 10 rooms deep
+     */
+    public static Iterator<RoomRiftSpace> create(ServerLevel level) {
+        return create(level, PortalSpawnLocation.DEFAULT_RIFT_EXIT_POSITION, DEFAULT_MAX_DEPTH);
+    }
+
+    /**
+     * @param level    The level to iterate rooms in
+     * @param maxDepth The maximum numbers of rooms deep to iterate
+     * @return An iterator over the rooms in the level, from the portal room
+     */
+    public static Iterator<RoomRiftSpace> create(ServerLevel level, int maxDepth) {
+        return create(level, PortalSpawnLocation.DEFAULT_RIFT_EXIT_POSITION, maxDepth);
+    }
+
+    /**
+     * @param level    The level to iterate rooms in
+     * @param startPos The position to iterate from
+     * @return An iterator over the rooms in the level, up to 10 rooms deep from the provided position
+     */
+    public static Iterator<RoomRiftSpace> create(ServerLevel level, BlockPos startPos) {
+        return create(level, startPos, DEFAULT_MAX_DEPTH);
+    }
+
+    /**
+     * @param level    The level to iterate rooms in
+     * @param startPos The section to iterate from
+     * @return An iterator over the rooms in the level, up to 10 rooms deep from the provided position
+     */
+    public static Iterator<RoomRiftSpace> create(ServerLevel level, SectionPos startPos) {
+        return create(level, startPos, DEFAULT_MAX_DEPTH);
+    }
+
+    /**
+     * @param level    The level to iterate rooms in
+     * @param startPos The position to iterate from
+     * @param depth    The number of rooms outwards to iterate to
+     * @return An iterator over the rooms in the level, from the provided position up to the provided depth of rooms
+     *         away
+     */
+    public static Iterator<RoomRiftSpace> create(ServerLevel level, BlockPos startPos, int depth) {
+        return create(level, SectionPos.of(startPos), depth);
+    }
+
+    /**
+     * @param level    The level to iterate rooms in
+     * @param startPos The position to iterate from
+     * @param depth    The number of rooms outwards to iterate to
+     * @return An iterator over the rooms in the level, from the provided position up to the provided depth of rooms
+     *         away
+     */
+    public static Iterator<RoomRiftSpace> create(ServerLevel level, SectionPos startPos, int depth) {
+        if (!(level.getChunkSource().getGenerator() instanceof FastRiftGenerator generator)) {
+            return Collections.emptyIterator();
+        }
+        RiftLayout layout = generator.getOrCreateLayout(level.getServer());
+        return new ConnectedRoomIterator(layout, startPos, depth);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !currentLayer.isEmpty() || !nextLayer.isEmpty();
+    }
+
+    @Override
+    public RoomRiftSpace next() {
+        RoomRiftSpace result = currentLayer.pop();
+        if (depth < maxDepth) {
+            result.corridors().forEach(corridor -> {
+                RiftSpace adjSpace = layout.getChunkSpace(corridor.getConnectingPos(result));
+                if (adjSpace instanceof RoomRiftSpace adjRoom && visitedRooms.add(adjSpace.origin())) {
+                    nextLayer.add(adjRoom);
+                }
+            });
+        }
+        if (currentLayer.isEmpty()) {
+            Deque<RoomRiftSpace> prev = currentLayer;
+            currentLayer = nextLayer;
+            nextLayer = prev;
+            depth++;
+        }
+        return result;
+    }
+}
