de18eb48c846de574bf1b856dda02a49eb47b34b
implementslegend <thsmpsns50@gmail.com>
2025-12-19 22:22:34 +0100

    feat: adjusted attack speed of tools to account for new default attack speed

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinToolMaterial.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinToolMaterial.java
@@ -1,10 +1,15 @@
 package com.wanderersoftherift.wotr.mixin;
 
+import net.minecraft.core.Holder;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ToolMaterial.class)
@@ -16,5 +21,15 @@ public class MixinToolMaterial {
             float attackSpeed,
             CallbackInfoReturnable<ItemAttributeModifiers> cir) {
         cir.setReturnValue(ItemAttributeModifiers.EMPTY);
+    }
+
+    @Redirect(method = "createToolAttributes", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/component/ItemAttributeModifiers$Builder;add(Lnet/minecraft/core/Holder;Lnet/minecraft/world/entity/ai/attributes/AttributeModifier;Lnet/minecraft/world/entity/EquipmentSlotGroup;)Lnet/minecraft/world/item/component/ItemAttributeModifiers$Builder;", ordinal = 1))
+    private ItemAttributeModifiers.Builder fasterOtherAttackSpeed(
+            ItemAttributeModifiers.Builder instance,
+            Holder<Attribute> attribute,
+            AttributeModifier modifier,
+            EquipmentSlotGroup slot) {
+        return instance.add(attribute,
+                new AttributeModifier(modifier.id(), modifier.amount() + 3, modifier.operation()), slot);
     }
 }
