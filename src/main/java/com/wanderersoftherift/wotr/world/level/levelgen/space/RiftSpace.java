6e6922184d120991daacf6b21a7e3d13d1cdfa10
immortius <immortius@gmail.com>
2025-12-28 14:38:26 +1100

    feat: experimental dungeon layout for rifts

--- src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/space/RiftSpace.java
+++ src/main/java/com/wanderersoftherift/wotr/world/level/levelgen/space/RiftSpace.java
@@ -6,7 +6,9 @@ import com.wanderersoftherift.wotr.world.level.levelgen.template.RiftGeneratable
 import net.minecraft.core.Vec3i;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * This describes volume allocated for room to generate in
@@ -24,4 +26,26 @@ public interface RiftSpace {
     TripleMirror templateTransform();
 
     @Nullable RiftGeneratable template();
+
+    default void forEachSection(Consumer<Vec3i> consumer) {
+        for (int z = 0; z < size().getZ(); z++) {
+            for (int y = 0; y < size().getY(); y++) {
+                for (int x = 0; x < size().getX(); x++) {
+                    consumer.accept(origin().offset(x, y, z));
+                }
+            }
+        }
+    }
+
+    default List<Vec3i> sections() {
+        List<Vec3i> result = new ArrayList<>();
+        for (int z = 0; z < size().getZ(); z++) {
+            for (int y = 0; y < size().getY(); y++) {
+                for (int x = 0; x < size().getX(); x++) {
+                    result.add(origin().offset(x, y, z));
+                }
+            }
+        }
+        return result;
+    }
 }
