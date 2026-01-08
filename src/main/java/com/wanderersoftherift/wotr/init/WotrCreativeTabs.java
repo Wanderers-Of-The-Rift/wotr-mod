0a605323c654350df560bc96150beb34ef21f9b2
Arnaud Debray <debray.arnaud@gmail.com>
2026-01-08 12:00:23 +0100

    feat: add buckets to dev creative tab

--- src/main/java/com/wanderersoftherift/wotr/init/WotrCreativeTabs.java
+++ src/main/java/com/wanderersoftherift/wotr/init/WotrCreativeTabs.java
@@ -108,6 +108,7 @@ public class WotrCreativeTabs {
                         output.accept(WotrItems.NOIR_HELMET);
                         output.accept(WotrItems.COLOR_HELMET);
                         WotrItems.DEV_BLOCK_ITEMS.forEach(item -> output.accept(item.get()));
+                        WotrFluids.fluidMap.values().forEach(wotrFluid -> output.accept(wotrFluid.FLUID_BUCKET));
                     })
                     .build());
 
