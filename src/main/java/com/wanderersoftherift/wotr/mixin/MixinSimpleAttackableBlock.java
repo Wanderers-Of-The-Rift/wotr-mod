f8e8a80ca97c516f60641cd3053796017a5ee8db
implementslegend <thsmpsns50@gmail.com>
2026-02-02 18:56:13 +0100

    feat: made some vanilla blocks attackable

---
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinSimpleAttackableBlock.java
@@ -0,0 +1,26 @@
+package com.wanderersoftherift.wotr.mixin;
+
+import com.wanderersoftherift.wotr.block.AttackableBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.BambooStalkBlock;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.WebBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin({ BushBlock.class, WebBlock.class, LeavesBlock.class, BambooStalkBlock.class })
+public class MixinSimpleAttackableBlock implements AttackableBlock {
+    @Override
+    public boolean isAttackable(BlockState state, BlockGetter level, BlockPos position) {
+        return true;
+    }
+
+    @Override
+    public void attack(BlockState state, LevelAccessor level, BlockPos position, float damage, DamageSource source) {
+        level.destroyBlock(position, true, source.getEntity());
+    }
+}
