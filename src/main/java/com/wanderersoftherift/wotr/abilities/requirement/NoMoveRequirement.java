6ea9e4a7a18ce68cef6135e0f6cb3313296c6690
Wout Stadman <48571034+cptflavio@users.noreply.github.com>
2026-02-04 01:29:53 +0100

    Simplify dY calculation

--- src/main/java/com/wanderersoftherift/wotr/abilities/requirement/NoMoveRequirement.java
+++ src/main/java/com/wanderersoftherift/wotr/abilities/requirement/NoMoveRequirement.java
@@ -23,13 +23,10 @@ public record NoMoveRequirement(float maxSpeed, boolean includeY) implements Abi
         if (context.level().isClientSide()) {
             return true;
         }
+    
         double dY;
-
-        if (!includeY) {
-            dY = 0;
-        } else {
-            dY = context.caster().yo - context.caster().yOld;
-        }
+        dY = (!includeY) ? 0 : context.caster().yo - context.caster().yOld;
+    
         Vec3 delta = new Vec3(
                 context.caster().xo - context.caster().xOld, dY, context.caster().zo - context.caster().zOld
         );
